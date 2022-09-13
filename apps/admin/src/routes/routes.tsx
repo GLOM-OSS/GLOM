d7dd3fba4f1ae5a46bc8778b0cef0a2222dfa53a
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-13 18:49:17 +0100

    configured signin route in admin app

--- apps/admin/src/routes/routes.tsx
+++ apps/admin/src/routes/routes.tsx
@@ -1,10 +1,15 @@
 import { Typography } from '@mui/material';
 import { Navigate } from 'react-router';
+import Signin from '../pages/signin';
 
 export const routes = [
   {
     path: '/',
     element: <Typography variant="h1">Welcome to Squoolr Admin</Typography>,
+  },
+  {
+    path: '/signin',
+    element: <Signin />,
   },
   { path: '*', element: <Navigate to="/" /> },
 ];
