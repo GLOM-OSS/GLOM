4da198afecd124071ddca1297639d3aaa0c489df
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-13 20:40:22 +0100

    added translations for signin lib

--- apps/admin/src/languages/en-us/website.ts
+++ apps/admin/src/languages/en-us/website.ts
@@ -1,3 +1,14 @@
 export const website = {
-    worldText:'World'
+  enterEmail: 'Enter your email',
+  email: 'Email',
+  enterCredentials: 'Enter your credentials to access your account',
+  welcomeBack: 'Welcome back',
+  password: 'Password',
+  enterPassword: 'Enter your password',
+  signin: 'Sign In',
+  forgotPassword: 'Forgot your password? ',
+  resetPassword: 'Reset Password',
+  signingUserIn: 'Please wait while we sign you in',
+  signinSuccess: 'Sign in successfull, we wish you a good work session!',
+  signinFailed: 'Something went wrong while signing you in. Please try again',
 };
