e7a2c47948ac96a41749d13081d416d8ddfb59fc
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-13 20:23:31 +0100

    updated notification and Error message in authentication lib and admin app to render caption sized notifications

--- libs/authentication/src/lib/ErrorMessage.tsx
+++ libs/authentication/src/lib/ErrorMessage.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Typography } from '@mui/material';
 import { theme } from '@squoolr/theme';
-import useNotification from '../utils/notification';
+import useNotification from './notification';
 
 export const ErrorMessage = ({
   retryFunction,
