37ffd802aa03afb78b9ef7bade2cd8b382dd162f
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-13 08:54:32 +0100

    updated fonts in document

--- apps/landing/pages/_document.tsx
+++ apps/landing/pages/_document.tsx
@@ -10,9 +10,11 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap"
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
         </Head>
         <body>
