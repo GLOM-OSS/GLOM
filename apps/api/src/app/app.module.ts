d762fee0ec7df60a4a1b2396336bba2bf0d21cb0
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-13 13:32:11 +0100

    minimal configuration for api app

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,54 @@
-import { Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import * as connectRedis from 'connect-redis';
+import { randomUUID } from 'crypto';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import { createClient } from 'redis';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    PassportModule.register({
+      session: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    const redisClient = createClient({ legacyMode: true });
+    redisClient.connect().catch((message) => Logger.error(message));
+    const RedisStore = connectRedis(session);
+
+    consumer
+      .apply(
+        session({
+          name: process.env.SESSION_NAME,
+          store: new RedisStore({
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            client: redisClient,
+            host: process.env.REDIS_HOST,
+            port: Number(process.env.REDIS_PORT),
+          }),
+          secret: process.env.SESSION_SECRET,
+          genid: () => randomUUID(),
+          saveUninitialized: false,
+          resave: false,
+          rolling: true,
+          cookie: {
+            maxAge: 10 * 60 * 1000, //10 minutes of inativity
+          },
+        }),
+        passport.initialize(),
+        passport.session()
+      )
+      .forRoutes('*');
+  }
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,20 +3,37 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: {
+      origin: '*',
+      credentials: true,
+    },
+  });
+  app.useStaticAssets('./assets');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+    })
   );
+  const config = new DocumentBuilder()
+    .setTitle('Squoolr APIs')
+    .setDescription('Detailed description of the services used on Squoolr.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.APP_PORT || 3333;
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
