932ec81ba03b76abd5ab3cbcdec24812fa254953
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-14 00:59:58 +0100

    added forgotPassword and new Password pages while configuring routes and translations files

--- apps/admin/src/languages/en-us/website.ts
+++ apps/admin/src/languages/en-us/website.ts
@@ -12,5 +12,21 @@ export const website = {
   signinSuccess: 'Sign in successfull, we wish you a good work session!',
   signinFailed: 'Something went wrong while signing you in. Please try again',
   retry: 'Retry',
-  close: 'Close'
+  close: 'Close',
+  confirmPassword: 'Confirm password',
+  passwordMismatch: 'Passwords must match',
+  resetPasswordSuccess: 'Password changed successfully',
+  changingPassword: 'Please wait while we change your password',
+  swapPasswordFailure:
+    'Something went wrong while trying to change your password. Please retry!',
+  swapPassword: 'Change your password to access your acount again',
+  change: 'Change',
+  rememberedPassword: 'Remembered password?',
+  sendingLink: 'Sending link',
+  linkSent: 'Link sent to your email',
+  failedToSendLink:
+    'Something went wrong while we tried to send link. Please try again!',
+  sendLinkSubtitle: 'Enter your email below and receive reset link',
+  sendLink: 'Send link',
+  verifyMail: 'Verify your mail box for link and reset password',
 };
--- apps/admin/src/routes/routes.tsx
+++ apps/admin/src/routes/routes.tsx
@@ -1,11 +1,21 @@
 import { Typography } from '@mui/material';
 import { Navigate } from 'react-router';
+import ForgotPassword from '../pages/forgotPassword';
+import NewPassword from '../pages/newPassword';
 import SigninPage from '../pages/signin';
 
 export const routes = [
   {
     path: '/',
     element: <SigninPage />,
+  },
+  {
+    path: '/forgot-password',
+    element: <ForgotPassword />,
+  },
+  {
+    path: 'reset-password/:code/new-password',
+    element: <NewPassword />,
   },
   {
     path: '/signin',
