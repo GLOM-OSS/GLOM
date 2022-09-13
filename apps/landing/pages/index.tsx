d93a0796226338d54536fc9f38e8e9a8c0c034d7
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-13 18:48:53 +0100

    brought landing app to base level

--- apps/landing/pages/index.tsx
+++ apps/landing/pages/index.tsx
@@ -1,17 +1,6 @@
-import { Box, Button, Typography } from '@mui/material';
-import { Authentication } from '@squoolr/auth';
 import { injectIntl, IntlShape } from 'react-intl';
-export function Index({ intl: { formatMessage }, intl }: { intl: IntlShape }) {
-  return (
-    <Box>
-      <Typography sx={{ fontFamily: 'Poppins' }}>Hello world</Typography>
-      <Typography sx={{ fontFamily: 'Roboto' }}>Hello world</Typography>
-      <Button color="secondary" variant="contained">
-        {formatMessage({ id: 'worldText' })}
-      </Button>
-      <Authentication intl={intl} user="Landing" />
-    </Box>
-  );
+export function Index({ intl }: { intl: IntlShape }) {
+  return <div>Welcome to Squoolr landing</div>;
 }
 
 export default injectIntl(Index);
