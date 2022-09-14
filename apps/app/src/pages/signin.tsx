c87dfb2b1f3921fb92283337bc964419ce889904
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-14 17:04:39 +0100

    adjusted personnel app. signin page to personnel callingApp

--- apps/app/src/pages/signin.tsx
+++ apps/app/src/pages/signin.tsx
@@ -2,7 +2,7 @@ import { Signin } from '@squoolr/auth';
 import { injectIntl, IntlShape } from 'react-intl';
 
 function SigninPage({ intl }: { intl: IntlShape }) {
-  return <Signin callingApp="admin" intl={intl} />;
+  return <Signin callingApp="personnel" intl={intl} />;
 }
 
 export default injectIntl(SigninPage);
