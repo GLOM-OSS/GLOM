48a1450c64ee59ea99aa7e502b3d3018810ccae3
Marco Kuidja <kuidjamarco@gmail.com>
2022-09-14 19:31:55 +0100

    added session serializer and session deserializer

---
+++ apps/api/src/app/auth/auth.serializer.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@nestjs/common';
+import { PassportSerializer } from '@nestjs/passport';
+
+import { AnnualConfiguratorService } from '../../services/annual-configurator.service';
+import { AnnualRegistryService } from '../../services/annual-registry.service';
+import { AnnualTeacherService } from '../../services/annual-teacher.service';
+import { StudentService } from '../../services/student.service';
+import {
+  DeserializeSessionData,
+  RecordValue,
+  Role,
+  SerializeSessionData,
+  UserRole,
+} from '../../utils/types';
+
+@Injectable()
+export class AuthSerializer extends PassportSerializer {
+  constructor(
+    private studentService: StudentService,
+    private annualTeacherService: AnnualTeacherService,
+    private annualRegistryService: AnnualRegistryService,
+    private AnnualConfiguratorService: AnnualConfiguratorService
+  ) {
+    super();
+  }
+
+  serializeUser(
+    user: Record<string, RecordValue>,
+    done: (err, user: SerializeSessionData) => void
+  ) {
+    done(null, {
+      login_id: user['login_id'] as string,
+      roles: user['roles'] as UserRole[],
+    });
+  }
+
+  async deserializeUser(
+    user: SerializeSessionData,
+    done: (err, user: DeserializeSessionData) => void
+  ) {
+    const { login_id, roles } = user;
+    let deserialedUser: DeserializeSessionData = { login_id };
+    for (let i = 0; i < roles.length; i++) {
+      const { user_id, role } = roles[i];
+      switch (role) {
+        case Role.CONFIGURATOR: {
+          const annualConfigurator =
+            await this.AnnualConfiguratorService.findUnique({
+              annual_configurator_id: user_id,
+            });
+          deserialedUser = { ...deserialedUser, annualConfigurator };
+          break;
+        }
+        case Role.REGISTRY: {
+          const annualRegistry = await this.annualRegistryService.findUnique({
+            annual_registry_id: user_id,
+          });
+          deserialedUser = { ...deserialedUser, annualRegistry };
+          break;
+        }
+        case Role.STUDENT: {
+          deserialedUser = await this.studentService.findUnique({
+            student_id: user_id,
+          });
+          break;
+        }
+        case Role.TEACHER: {
+          const annualTeacher = await this.annualTeacherService.findUnique({
+            annual_teacher_id: user_id,
+          });
+          deserialedUser = { ...deserialedUser, annualTeacher };
+          break;
+        }
+      }
+    }
+    done(null, deserialedUser);
+  }
+}
--- apps/api/src/utils/types.ts
+++ apps/api/src/utils/types.ts
@@ -7,25 +7,49 @@ import {
   Student,
 } from '@prisma/client';
 
-export type RecordValue = number | string | Date | Lang | Gender | boolean;
-export enum UserRole {
-  ADMIN = 'ADMIN',
+export type RecordValue =
+  | number
+  | string
+  | Date
+  | Lang
+  | Gender
+  | boolean
+  | UserRole[];
+export enum Role {
   STUDENT = 'STUDENT',
   TEACHER = 'TEACHER',
   REGISTRY = 'REGISTRY',
   CONFIGURATOR = 'CONFIGURATOR',
 }
+export type UserRole = {
+  user_id: string;
+  role: Role;
+};
 
 export type SerializeSessionData = {
-  user_id: string;
-  role?: UserRole;
+  login_id: string;
+  roles: UserRole[];
 };
 
 export type DeserializeSessionData = {
   login_id: string; //admin,
-  student?: Student; //Student
+  annualStudent?: Student; //Student
   //Personnel
   annualConfigurator?: AnnualConfigurator;
   annualTeacher?: AnnualTeacher;
   annualRegistry?: AnnualRegistry;
 };
+
+declare module 'express-session' {
+  interface SessionData {
+    views: number;
+    SessionData: {
+      login_id: string; //admin,
+      student?: Student; //Student
+      //Personnel
+      annualConfigurator?: AnnualConfigurator;
+      annualTeacher?: AnnualTeacher;
+      annualRegistry?: AnnualRegistry;
+    };
+  }
+}
