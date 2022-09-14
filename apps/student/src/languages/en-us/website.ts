5e9ceeeaa753a1c9c746dbd902d195eabd8bece3
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-14 01:09:37 +0100

    configured authentication routes in student app

--- apps/student/src/languages/en-us/website.ts
+++ apps/student/src/languages/en-us/website.ts
@@ -1,3 +1,32 @@
 export const website = {
-    worldText:'World'
+  enterEmail: 'Enter your email',
+  email: 'Email',
+  enterCredentials: 'Enter your credentials to access your account',
+  welcomeBack: 'Welcome back',
+  password: 'Password',
+  enterPassword: 'Enter your password',
+  signin: 'Sign In',
+  forgotPassword: 'Forgot your password? ',
+  resetPassword: 'Reset Password',
+  signingUserIn: 'Please wait while we sign you in',
+  signinSuccess: 'Sign in successfull, we wish you a good work session!',
+  signinFailed: 'Something went wrong while signing you in. Please try again',
+  retry: 'Retry',
+  close: 'Close',
+  confirmPassword: 'Confirm password',
+  passwordMismatch: 'Passwords must match',
+  resetPasswordSuccess: 'Password changed successfully',
+  changingPassword: 'Please wait while we change your password',
+  swapPasswordFailure:
+    'Something went wrong while trying to change your password. Please retry!',
+  swapPassword: 'Change your password to access your acount again',
+  change: 'Change',
+  rememberedPassword: 'Remembered password?',
+  sendingLink: 'Sending link',
+  linkSent: 'Link sent to your email',
+  failedToSendLink:
+    'Something went wrong while we tried to send link. Please try again!',
+  sendLinkSubtitle: 'Enter your email below and receive reset link',
+  sendLink: 'Send link',
+  verifyMail: 'Verify your mail box for link and reset password',
 };
--- apps/student/src/routes/routes.tsx
+++ apps/student/src/routes/routes.tsx
@@ -1,9 +1,24 @@
 import { Typography } from '@mui/material';
 import { Navigate } from 'react-router';
+import ForgotPassword from '../pages/forgotPassword';
+import NewPassword from '../pages/newPassword';
+import SigninPage from '../pages/signin'
 
 export const routes = [
   {
     path: '/',
+    element: <SigninPage />,
+  },
+  {
+    path: '/forgot-password',
+    element: <ForgotPassword />,
+  },
+  {
+    path: 'reset-password/:code/new-password',
+    element: <NewPassword />,
+  },
+  {
+    path: '/dashboard',
     element: <Typography variant="h1">Welcome to Squoolr for Students</Typography>,
   },
   { path: '*', element: <Navigate to="/" /> },
