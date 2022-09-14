45e5dbe888cf35f48e0feb8db61952f5cc625978
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-14 16:25:51 +0100

    configured selectAcademicYear on Signin lib for student and personnel

--- libs/authentication/src/lib/signin.tsx
+++ libs/authentication/src/lib/signin.tsx
@@ -19,6 +19,10 @@ import { useNavigate } from 'react-router';
 import favicon from './logo.png';
 import { ErrorMessage, useNotification } from '@squoolr/toast';
 import { random } from '@squoolr/utils';
+import {
+  AcademicYearInterface,
+  SelectAcademicYearDialog,
+} from './selectAcademicYear';
 
 export function Signin({
   intl: { formatMessage },
@@ -37,6 +41,11 @@ export function Signin({
     password: Yup.string().required(),
   });
   const navigate = useNavigate();
+  const [isAcademicYearDialogOpen, setIsAcademicYearDialogOpen] =
+    useState<boolean>(false);
+  const [academicYears, setAcademicYears] = useState<AcademicYearInterface[]>(
+    []
+  );
 
   const [notifications, setNotifications] = useState<useNotification[]>();
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
@@ -61,7 +70,43 @@ export function Signin({
           newNotification.update({
             render: formatMessage({ id: 'signinSuccess' }),
           });
-          navigate(localStorage.getItem('previousRoute') ?? '/dashboard');
+          if (callingApp === 'admin') {
+            navigate(localStorage.getItem('previousRoute') ?? '/dashboard');
+          } else {
+            //TODO: SET DATA IN ACADEMIC YEAR STATE HERE
+            const newAcademicYears: AcademicYearInterface[] = [
+              {
+                academic_year_id: 'https:qwoeiofs/sldie',
+                code: 'YEAR-202100012022',
+                end_date: new Date(),
+                starting_date: new Date(),
+                year_status: 'inactive',
+              },
+              {
+                academic_year_id: 'https:qwwoeios/sldie',
+                code: 'YEAR-1',
+                end_date: new Date(),
+                starting_date: new Date(),
+                year_status: 'inactive',
+              },
+              {
+                academic_year_id: 'https:qwoerios/sldie',
+                code: 'YEAR-1',
+                end_date: new Date(),
+                starting_date: new Date(),
+                year_status: 'active',
+              },
+              {
+                academic_year_id: 'https:qwoeios/sldige',
+                code: 'YEAR-1',
+                end_date: new Date(),
+                starting_date: new Date(),
+                year_status: 'finished',
+              },
+            ];
+            setAcademicYears(newAcademicYears);
+            setIsAcademicYearDialogOpen(true);
+          }
           resetForm();
         } else {
           newNotification.update({
@@ -83,109 +128,123 @@ export function Signin({
   });
 
   return (
-    <Box
-      sx={{
-        backgroundColor: theme.common.background,
-        width: '100vw',
-        minHeight: '100vh',
-        color: theme.common.titleActive,
-        display: 'grid',
-        justifyContent: 'center',
-        alignContent: 'center',
-      }}
-    >
-      <img src={favicon} alt="Squoolr icon" style={{ justifySelf: 'center' }} />
+    <>
+      {callingApp !== 'admin' && (
+        <SelectAcademicYearDialog
+          academicYears={academicYears}
+          closeDialog={() => setIsAcademicYearDialogOpen(false)}
+          isDialogOpen={isAcademicYearDialogOpen}
+          intl={intl}
+        />
+      )}
       <Box
         sx={{
-          border: `2.5px solid ${theme.common.inputBackground}`,
-          padding: `${theme.spacing(5)} ${theme.spacing(7.5)}`,
+          backgroundColor: theme.common.background,
+          width: '100vw',
+          minHeight: '100vh',
+          color: theme.common.titleActive,
           display: 'grid',
-          justifyItems: 'center',
-          margin: `${theme.spacing(6.25)} 0`,
+          justifyContent: 'center',
+          alignContent: 'center',
         }}
       >
-        <Typography variant="h2">
-          {formatMessage({ id: 'welcomeBack' })}
-        </Typography>
-        <Typography sx={{ marginBottom: theme.spacing(6.25) }}>
-          {formatMessage({
-            id: 'enterCredentials',
-          })}
-        </Typography>
-        <form onSubmit={formik.handleSubmit}>
-          <TextField
-            fullWidth
-            required
-            autoFocus
-            label={formatMessage({ id: 'email' })}
-            placeholder={formatMessage({ id: 'enterEmail' })}
-            variant="outlined"
-            type="email"
-            error={formik.touched.email && Boolean(formik.errors.email)}
-            helperText={formik.touched.email && formik.errors.email}
-            {...formik.getFieldProps('email')}
-            disabled={isSubmitting}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <EmailRounded color="primary" />
-                </InputAdornment>
-              ),
-            }}
-          />
-          <TextField
-            fullWidth
-            required
-            label={formatMessage({ id: 'password' })}
-            placeholder={formatMessage({ id: 'enterPassword' })}
-            variant="outlined"
-            type="password"
-            error={formik.touched.password && Boolean(formik.errors.password)}
-            helperText={formik.touched.password && formik.errors.password}
-            {...formik.getFieldProps('password')}
-            disabled={isSubmitting}
-            sx={{ marginTop: theme.spacing(3.125) }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <LockPersonRounded color="primary" />
-                </InputAdornment>
-              ),
-            }}
-          />
-          <Button
-            color="primary"
-            variant="contained"
-            size="large"
-            fullWidth
-            type="submit"
-            disabled={
-              isSubmitting ||
-              formik.values.password === '' ||
-              formik.values.email === ''
-            }
-            sx={{ marginTop: theme.spacing(6.25), textTransform: 'none' }}
-          >
-            {formatMessage({ id: 'signin' })}
-          </Button>
-        </form>
-      </Box>
-      {callingApp !== 'personnel' && (
-        <Typography
-          sx={{ color: theme.common.placeholder, justifySelf: 'center' }}
-          component="span"
+        <img
+          src={favicon}
+          alt="Squoolr icon"
+          style={{ justifySelf: 'center' }}
+        />
+        <Box
+          sx={{
+            border: `2.5px solid ${theme.common.inputBackground}`,
+            padding: `${theme.spacing(5)} ${theme.spacing(7.5)}`,
+            display: 'grid',
+            justifyItems: 'center',
+            margin: `${theme.spacing(6.25)} 0`,
+          }}
         >
-          {formatMessage({ id: 'forgotPassword' })}{' '}
+          <Typography variant="h2">
+            {formatMessage({ id: 'welcomeBack' })}
+          </Typography>
+          <Typography sx={{ marginBottom: theme.spacing(6.25) }}>
+            {formatMessage({
+              id: 'enterCredentials',
+            })}
+          </Typography>
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              fullWidth
+              required
+              autoFocus
+              label={formatMessage({ id: 'email' })}
+              placeholder={formatMessage({ id: 'enterEmail' })}
+              variant="outlined"
+              type="email"
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+              {...formik.getFieldProps('email')}
+              disabled={isSubmitting}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <EmailRounded color="primary" />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              fullWidth
+              required
+              label={formatMessage({ id: 'password' })}
+              placeholder={formatMessage({ id: 'enterPassword' })}
+              variant="outlined"
+              type="password"
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+              {...formik.getFieldProps('password')}
+              disabled={isSubmitting}
+              sx={{ marginTop: theme.spacing(3.125) }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockPersonRounded color="primary" />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Button
+              color="primary"
+              variant="contained"
+              size="large"
+              fullWidth
+              type="submit"
+              disabled={
+                isSubmitting ||
+                formik.values.password === '' ||
+                formik.values.email === ''
+              }
+              sx={{ marginTop: theme.spacing(6.25), textTransform: 'none' }}
+            >
+              {formatMessage({ id: 'signin' })}
+            </Button>
+          </form>
+        </Box>
+        {callingApp !== 'personnel' && (
           <Typography
-            sx={{ color: theme.palette.primary.main, cursor: 'pointer' }}
-            onClick={() => navigate('/forgot-password')}
+            sx={{ color: theme.common.placeholder, justifySelf: 'center' }}
             component="span"
           >
-            {formatMessage({ id: 'resetPassword' })}
+            {formatMessage({ id: 'forgotPassword' })}{' '}
+            <Typography
+              sx={{ color: theme.palette.primary.main, cursor: 'pointer' }}
+              onClick={() => navigate('/forgot-password')}
+              component="span"
+            >
+              {formatMessage({ id: 'resetPassword' })}
+            </Typography>
           </Typography>
-        </Typography>
-      )}
-    </Box>
+        )}
+      </Box>
+    </>
   );
 }
 
