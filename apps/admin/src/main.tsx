080cae8147904d09d91f1e1750ac151d1b5d1594
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-17 22:51:49 +0100

    deleted all files relative to language in admin app

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -2,15 +2,12 @@ import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app';
-import LanguageContextProvider from './contexts/language/LanguageContextProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <LanguageContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </LanguageContextProvider>
 );
