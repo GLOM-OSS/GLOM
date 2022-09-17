2cb115318948f12de8a2ce472451de56aeea7660
Marco Kuidja <kuidjamarco@gmail.com>
2022-09-18 00:48:27 +0100

    added api-services lib for requerying the api server

---
+++ libs/api-services/src/lib/auth/index.ts
@@ -0,0 +1,45 @@
+import { http } from '@squoolr/axios';
+
+export async function signIn(email: string, password: string) {
+  const { data } = await http.post(`auth/signin`, {
+    email,
+    password,
+  });
+  return data;
+}
+
+export async function setActiveYear(selected_academic_year_id: string) {
+  const { data } = await http.patch(`auth/active-password`, {
+    selected_academic_year_id,
+  });
+  return data;
+}
+
+export async function resetPassword(email: string) {
+  return http.post(`auth/reset-password`, { email });
+}
+
+export async function setNewPassword(
+  reset_password_id: string,
+  new_password: string
+) {
+  return http.post(`auth/new-password`, { reset_password_id, new_password });
+}
+
+export async function logOut() {
+  return await http.delete(`auth/log-out`);
+}
+
+export async function getUser() {
+  const {
+    data: { user },
+  } = await http.get(`auth/user`);
+  return user;
+}
+
+export async function googleSignIn() {
+  const {
+    data: { user },
+  } = await http.get(`auth/google-signin`);
+  return user;
+}
