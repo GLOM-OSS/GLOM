34957bb06c0aac447a9c1c059d5ffee4b1833e9a
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-18 14:05:35 +0100

    configured adminlayout

--- apps/admin/src/pages/AdminLayout.tsx
+++ apps/admin/src/pages/AdminLayout.tsx
@@ -1,34 +1,6 @@
-import { DashboardRounded, LayersOutlined, SettingsRounded } from '@mui/icons-material';
-import { Layout, NavItem, PersonnelRole, User } from '@squoolr/layout';
-import { useState } from 'react';
+import { LayersOutlined, SettingsSuggestOutlined } from '@mui/icons-material';
+import { MainLayout, NavItem } from '@squoolr/layout';
 import { injectIntl, IntlShape } from 'react-intl';
-
-//TODO: replace this data with the data from user context later on
-const user: User = {
-  activeYear: {
-    academic_year_id: 'hello world',
-    ending_date: new Date('2003-10-12'),
-    starting_date: new Date('2022-10-12'),
-    code: 'kskdls',
-    year_status: 'active',
-  },
-  birthdate: new Date('1999/03/27'),
-  email: 'lorraintchakoumi@gmail.com',
-  fisrt_name: 'Tchakoumi Lorrain',
-  gender: 'Female',
-  last_name: 'Kouatchoua',
-  login_id: 'disoeosenso',
-  national_id_number: '000316122',
-  person_id: 'wieo',
-  phone_number: '657140183',
-  preferred_lang: 'En',
-  annualConfigurator: { annual_configurator_id: 'lsk', is_sudo: false },
-  annualRegistry: { annual_registry_id: 'lsk' },
-};
-
-export const Testing2 = () => {
-  return <div>massa mi heeee</div>;
-};
 
 function AdminLayout({ intl: { formatMessage } }: { intl: IntlShape }) {
   const navItems: NavItem[] = [
@@ -56,25 +28,19 @@ function AdminLayout({ intl: { formatMessage } }: { intl: IntlShape }) {
     },
     {
       id: 2,
-      Icon: DashboardRounded,
+      Icon: SettingsSuggestOutlined,
       title: formatMessage({ id: 'settings' }),
       children: [],
     },
   ];
 
-  const [activeRole, setActiveRole] = useState<PersonnelRole | 'administrator'>(
-    'teacher'
-  ); //TODO: redefine initial value with roles
-  const handleSwapRole = (newRole: PersonnelRole) => setActiveRole(newRole);
-
   return (
-    <Layout
-      callingApp="admin"
-      activeRole={activeRole}
-      userRoles={[]}
-      user={user}
-      navItems={navItems}
-      handleSwapRole={handleSwapRole}
+    <MainLayout
+      callingApp="personnel"
+      navItems={[
+        { role: 'registry', navItems: navItems },
+        { role: 'secretary', navItems: [] },
+      ]}
     />
   );
 }
--- apps/admin/src/routes/routes.tsx
+++ apps/admin/src/routes/routes.tsx
@@ -1,6 +1,6 @@
 import { Typography } from '@mui/material';
 import { Navigate } from 'react-router';
-import AdminLayout, { Testing2 } from '../pages/AdminLayout';
+import AdminLayout from '../pages/AdminLayout';
 import ForgotPassword from '../pages/forgotPassword';
 import NewPassword from '../pages/newPassword';
 import SigninPage from '../pages/signin';
@@ -19,14 +19,31 @@ export const routes = [
     element: <NewPassword />,
   },
   {
-    path: '/layout',
+    path: '/management',
     element: <AdminLayout />,
     children: [
       {
-        path: 'dashboard',
-        element: <Typography variant="h1">Welcome to Squoolr Admin</Typography>,
+        path: 'demands',
+        element: <Typography variant="h1">Demands</Typography>,
       },
-      { path: 'test', element: <Testing2 /> },
+      {
+        path: 'schools',
+        element: <Typography variant="h1">Schools</Typography>,
+      },
+      {
+        path: 'configurators',
+        element: <Typography variant="h1">configurators</Typography>,
+      },
+    ],
+  },
+  {
+    path: 'settings',
+    element: <AdminLayout />,
+    children: [
+      {
+        path: '*',
+        element: <Typography variant="h1">Demands</Typography>,
+      },
     ],
   },
   {
