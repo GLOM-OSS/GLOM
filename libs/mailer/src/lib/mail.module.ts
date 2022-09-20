0adefc2945e0da0b3918de161a58d2b333078e76
Marco Kuidja <kuidjamarco@gmail.com>
2022-09-20 10:08:17 +0100

    added node mailer depencies

--- libs/mailer/src/lib/mail.module.ts
+++ libs/mailer/src/lib/mail.module.ts
@@ -1,10 +1,11 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { join } from 'path';
 import { MailService } from './mail.service';
 import { formatMessage } from './messages/helpers';
 
+@Global()
 @Module({
   imports: [
     MailerModule.forRoot({
