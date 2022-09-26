1aafca72334a8fd59aa643ef2c90b1bb09816d58
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-09-26 10:15:38 +0100

    added academic Item skeleton

--- apps/app/src/AcademicItem.tsx
+++ apps/app/src/AcademicItem.tsx
@@ -6,12 +6,83 @@ import {
   lighten,
   Menu,
   MenuItem,
+  Skeleton,
   Tooltip,
   Typography,
 } from '@mui/material';
 import { theme } from '@squoolr/theme';
+import { random } from '@squoolr/utils';
 import { useState } from 'react';
 import { useIntl } from 'react-intl';
+
+export const AcademicItemSkeleton = ({ hasChips }: { hasChips?: boolean }) => {
+  const { formatMessage } = useIntl();
+  return (
+    <Box
+      sx={{
+        padding: `${theme.spacing(2)} ${theme.spacing(2)}`,
+        backgroundColor: lighten(theme.palette.primary.main, 0.9),
+        borderRadius: '10px',
+        color: theme.common.titleActive,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: '1fr auto',
+          columnGap: theme.spacing(1.5),
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'auto 1fr',
+            columnGap: theme.spacing(1),
+            alignItems: 'center',
+          }}
+        >
+          <Typography
+            variant="caption"
+            sx={{ color: theme.common.placeholder }}
+          >{`${formatMessage({ id: 'createdOn' })} : `}</Typography>
+          <Typography variant="caption">
+            <Skeleton sx={{ width: `60%` }} animation="wave" />
+          </Typography>
+        </Box>
+        <IconButton>
+          <Tooltip arrow title={formatMessage({ id: 'more' })}>
+            <MoreVertRounded
+              sx={{ fontSize: '20px', color: theme.common.titleActive }}
+            />
+          </Tooltip>
+        </IconButton>
+      </Box>
+      <Typography sx={{ marginTop: theme.spacing(2) }}>
+        <Skeleton sx={{ width: `${random() * 10}%` }} animation="wave" />
+      </Typography>
+      {hasChips && (
+        <Box sx={{ marginTop: theme.spacing(2) }}>
+          {[...new Array(2)].map((item, index) => (
+            <Chip
+              size="small"
+              sx={{
+                justifySelf: 'center',
+                marginRight: theme.spacing(1),
+              }}
+              label={
+                <Skeleton
+                  sx={{ width: index === 0 ? '40px' : '120px' }}
+                  animation="wave"
+                />
+              }
+            />
+          ))}
+        </Box>
+      )}
+    </Box>
+  );
+};
 
 interface AcadItemInterface {
   created_at: Date;
