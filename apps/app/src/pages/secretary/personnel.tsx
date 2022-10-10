d9053be1f297b446bd9aa2cf52431a492b329093
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-10-10 01:42:25 +0100

    updated folder structure for personnel

--- apps/app/src/pages/secretary/personnel.tsx
+++ apps/app/src/pages/secretary/personnel.tsx
@@ -1,14 +1,25 @@
 import { Box } from '@mui/material';
+import { theme } from '@squoolr/theme';
 import { useState } from 'react';
+import ActionBar from '../../components/secretary/personnel/actionBar';
 import PersonnelTabs, {
   TabItem,
-} from '../../components/secretary/personnelTabs';
+} from '../../components/secretary/personnel/personnelTabs';
 
 export default function Personnel() {
   const [activeTabItem, setActiveTabItem] = useState<TabItem>('allPersonnel');
   return (
-    <Box>
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateRows: 'auto auto 1fr',
+        height: '100%',
+        gap: theme.spacing(2)
+      }}
+    >
       <PersonnelTabs setActiveTabItem={setActiveTabItem} />
+      <ActionBar />
+      <Box sx={{ height: '100%' }}>{activeTabItem}</Box>
     </Box>
   );
 }
