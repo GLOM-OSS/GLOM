1ef7f5ba804f64bf91e37db1fa542e426c7fa270
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-10-10 05:10:44 +0100

    configured personnel to load personnel and display the rows of personnels

--- apps/app/src/pages/secretary/personnel.tsx
+++ apps/app/src/pages/secretary/personnel.tsx
@@ -1,24 +1,136 @@
-import { Box } from '@mui/material';
+import { ReportRounded } from '@mui/icons-material';
+import {
+  Box,
+  lighten,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 import { theme } from '@squoolr/theme';
+import { ErrorMessage, useNotification } from '@squoolr/toast';
+import { random } from '@squoolr/utils';
 import { useEffect, useState } from 'react';
+import Scrollbars from 'react-custom-scrollbars-2';
+import { useIntl } from 'react-intl';
 import ActionBar from '../../components/secretary/personnel/actionBar';
+import PersonnelRow, {
+  PersonnelInterface,
+} from '../../components/secretary/personnel/PersonnelRow';
+import { PersonnelRowSkeleton } from '../../components/secretary/personnel/personnelRowSkeleton';
 import PersonnelTabs, {
   TabItem,
 } from '../../components/secretary/personnel/personnelTabs';
 
 export default function Personnel() {
+  const { formatMessage } = useIntl();
   const [activeTabItem, setActiveTabItem] = useState<TabItem>('allPersonnel');
   const [searchValue, setSearchValue] = useState<string>('');
   const [isAddNewPersonnelDialogOpen, setIsAddNewPersonnelDialogOPen] =
     useState<boolean>(false);
 
+  const [personnels, setPersonnels] = useState<PersonnelInterface[]>([]);
+  const [arePersonnelLoading, setArePersonnelLoading] =
+    useState<boolean>(false);
+
+  const [notifications, setNotifications] = useState<
+    {
+      usage: 'load' | 'edit' | 'password_reset' | 'code_reset';
+      notif: useNotification;
+    }[]
+  >([]);
+
   const getPersonnels = () => {
-    alert('changing value');
+    setArePersonnelLoading(true);
+
+    const notif = new useNotification();
+    const newNotifs = notifications.filter(({ usage, notif }) => {
+      if (usage === 'load') notif.dismiss();
+      return usage !== 'load';
+    });
+    setNotifications([...newNotifs, { usage: 'load', notif }]);
+
+    //TODO: call api here to load personnel with data activeTabItem or searchValue
+    setTimeout(() => {
+      if (random() > 5) {
+        const newPersonnels: PersonnelInterface[] = [
+          {
+            personnel_id: 'shess',
+            first_name: 'Kouatchoua',
+            last_name: 'Tchakoumi Lorrain',
+            email: 'lorraintchakoumi@gmail.com',
+            phone: '657140183',
+            last_connected: new Date(),
+            is_coordo: true,
+            is_academic_service: true,
+            is_teacher: true,
+            is_secretariat: true,
+          },
+          {
+            personnel_id: 'shses',
+            first_name: 'Kouatchoua',
+            last_name: 'Tchakoumi Lorrain',
+            email: 'lorraintchakoumi@gmail.com',
+            phone: '657140183',
+            last_connected: new Date(),
+            is_coordo: true,
+            is_academic_service: true,
+            is_teacher: true,
+            is_secretariat: true,
+          },
+          {
+            personnel_id: 'sshes',
+            first_name: 'Kouatchoua',
+            last_name: 'Tchakoumi Lorrain',
+            email: 'lorraintchakoumi@gmail.com',
+            phone: '657140183',
+            last_connected: new Date(),
+            is_coordo: true,
+            is_academic_service: true,
+            is_teacher: true,
+            is_secretariat: true,
+          },
+          {
+            personnel_id: 'sshess',
+            first_name: 'Kouatchoua',
+            last_name: 'Tchakoumi Lorrain',
+            email: 'lorraintchakoumi@gmail.com',
+            phone: '657140183',
+            last_connected: new Date(),
+            is_coordo: true,
+            is_academic_service: true,
+            is_teacher: true,
+            is_secretariat: true,
+          },
+        ];
+        setPersonnels(newPersonnels);
+        setArePersonnelLoading(false);
+        notif.dismiss();
+      } else {
+        notif.notify({ render: formatMessage({ id: 'loadingPersonnel' }) });
+        notif.update({
+          type: 'ERROR',
+          render: (
+            <ErrorMessage
+              retryFunction={getPersonnels}
+              notification={notif}
+              //TODO: MESSAGE SHOULD COME FROM BACKEND
+              message={formatMessage({ id: 'failedToLoadPersonnel' })}
+            />
+          ),
+          autoClose: false,
+          icon: () => <ReportRounded fontSize="medium" color="error" />,
+        });
+      }
+    }, 3000);
   };
 
   useEffect(() => {
     getPersonnels();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchValue, activeTabItem]);
+
   return (
     <Box
       sx={{
@@ -37,7 +149,46 @@ export default function Personnel() {
             : setIsAddNewPersonnelDialogOPen(true)
         }
       />
-      <Box sx={{ height: '100%' }}>{activeTabItem}</Box>
+      <Scrollbars>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead
+            sx={{ backgroundColor: lighten(theme.palette.primary.main, 0.8) }}
+          >
+            <TableRow>
+              {['personnelName', 'email', 'phone', 'lastConnect', 'role'].map(
+                (value, index) => (
+                  <TableCell
+                    key={index}
+                    sx={{
+                      ...theme.typography.body1,
+                      color: theme.common.body,
+                      fontWeight: 300,
+                    }}
+                  >
+                    {formatMessage({ id: value })}
+                  </TableCell>
+                )
+              )}
+              <TableCell />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {arePersonnelLoading &&
+              [...new Array(10)].map((_, index) => (
+                <PersonnelRowSkeleton index={index} key={index} />
+              ))}
+            {!arePersonnelLoading &&
+              personnels.map((personnel, index) => (
+                <PersonnelRow
+                  index={index}
+                  personnel={personnel}
+                  key={index}
+                  activeTabItem={activeTabItem}
+                />
+              ))}
+          </TableBody>
+        </Table>
+      </Scrollbars>
     </Box>
   );
 }
