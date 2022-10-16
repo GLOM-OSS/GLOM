2ee3dfb0a2015c25a028d71ea88d4c1d58b4e316
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-10-16 06:56:08 +0100

    changed display of classrooms, display excluded classrooms as a set, added display secion when not active

--- apps/app/src/components/secretary/newAcademicYear/classrooms.tsx
+++ apps/app/src/components/secretary/newAcademicYear/classrooms.tsx
@@ -3,12 +3,14 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
+  Box,
   Checkbox,
   FormControlLabel,
   FormGroup,
   Grid,
   Typography,
 } from '@mui/material';
+import { theme } from '@squoolr/theme';
 import { useState } from 'react';
 import { useIntl } from 'react-intl';
 import { ClassroomInterface } from '../classrooms';
@@ -75,7 +77,9 @@ export default function Classrooms({
   classrooms,
   selectedClassroomCodes,
   setSelectedClassroomCodes,
+  isActive,
 }: {
+  isActive: boolean;
   classrooms: ClassroomInterface[];
   selectedClassroomCodes: string[];
   setSelectedClassroomCodes: (selectedClassrooms: string[]) => void;
@@ -101,52 +105,148 @@ export default function Classrooms({
       });
     }
   });
+
+  const { formatMessage } = useIntl();
+
+  //TODO:   replace major_name by major_id if above point is accepted
+  let excludedClassrooms: {
+    major_name: string;
+    classroomLevels: number[];
+  }[] = [];
+  classrooms.forEach((classroom) => {
+    if (!selectedClassroomCodes.includes(classroom.classroom_code)) {
+      const classroomMajor = excludedClassrooms.find(
+        ({ major_name: mn }) => mn === classroom.classroom_name
+      );
+      if (classroomMajor) {
+        if (
+          !classroomMajor.classroomLevels.includes(classroom.classroom_level)
+        ) {
+          const newExcludedClassrooms = excludedClassrooms.filter(
+            ({ major_name }) => major_name !== classroom.classroom_name
+          );
+          excludedClassrooms = [
+            ...newExcludedClassrooms,
+            {
+              ...classroomMajor,
+              classroomLevels: [
+                ...classroomMajor.classroomLevels,
+                classroom.classroom_level,
+              ],
+            },
+          ];
+        }
+      } else {
+        excludedClassrooms.push({
+          major_name: classroom.classroom_name,
+          classroomLevels: [classroom.classroom_level],
+        });
+      }
+    }
+  });
+
   return (
-    <Grid container spacing={2}>
-      {distinctMajors
-        .sort((a, b) => (a.major_name > b.major_name ? 1 : -1))
-        .map((major, index) => {
-          const checked = major.classrooms.map(({ classroom_code: cc }) =>
-            selectedClassroomCodes.includes(cc)
-          );
+    <>
+      {isActive && (
+        <Grid container spacing={2}>
+          {distinctMajors
+            .sort((a, b) => (a.major_name > b.major_name ? 1 : -1))
+            .map((major, index) => {
+              const checked = major.classrooms.map(({ classroom_code: cc }) =>
+                selectedClassroomCodes.includes(cc)
+              );
 
-          return (
-            <MajorDisplay
-              major={major}
-              key={index}
-              checked={!checked.includes(false)}
-              indeterminate={
-                checked.length > 0 &&
-                checked.includes(true) &&
-                checked.includes(false)
-              }
-              onMajorClick={() => {
-                let newSelected = selectedClassroomCodes;
-                if (checked.includes(false)) {
-                  major.classrooms.forEach(({ classroom_code: cc }) => {
-                    if (!newSelected.includes(cc)) newSelected.push(cc);
-                  });
-                  setSelectedClassroomCodes(newSelected);
-                } else {
-                  major.classrooms.forEach(({ classroom_code: cc }) => {
-                    newSelected = newSelected.filter((_) => _ !== cc);
-                  });
+              return (
+                <MajorDisplay
+                  major={major}
+                  key={index}
+                  checked={!checked.includes(false)}
+                  indeterminate={
+                    checked.length > 0 &&
+                    checked.includes(true) &&
+                    checked.includes(false)
+                  }
+                  onMajorClick={() => {
+                    let newSelected = selectedClassroomCodes;
+                    if (checked.includes(false)) {
+                      major.classrooms.forEach(({ classroom_code: cc }) => {
+                        if (!newSelected.includes(cc)) newSelected.push(cc);
+                      });
+                      setSelectedClassroomCodes(newSelected);
+                    } else {
+                      major.classrooms.forEach(({ classroom_code: cc }) => {
+                        newSelected = newSelected.filter((_) => _ !== cc);
+                      });
 
-                  setSelectedClassroomCodes(newSelected);
-                }
-              }}
-              selectClassroom={(cc: string) => {
-                if (selectedClassroomCodes.includes(cc)) {
-                  setSelectedClassroomCodes(
-                    selectedClassroomCodes.filter((_) => _ !== cc)
-                  );
-                } else
-                  setSelectedClassroomCodes([...selectedClassroomCodes, cc]);
-              }}
-              selectedClassroomCodes={selectedClassroomCodes}
-            />
-          );
-        })}
-    </Grid>
+                      setSelectedClassroomCodes(newSelected);
+                    }
+                  }}
+                  selectClassroom={(cc: string) => {
+                    if (selectedClassroomCodes.includes(cc)) {
+                      setSelectedClassroomCodes(
+                        selectedClassroomCodes.filter((_) => _ !== cc)
+                      );
+                    } else
+                      setSelectedClassroomCodes([
+                        ...selectedClassroomCodes,
+                        cc,
+                      ]);
+                  }}
+                  selectedClassroomCodes={selectedClassroomCodes}
+                />
+              );
+            })}
+        </Grid>
+      )}
+      {!isActive && (
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'auto 1fr',
+            gap: theme.spacing(1),
+          }}
+        >
+          <Typography sx={{ color: theme.common.placeholder }}>
+            {`${formatMessage({ id: 'academicServiceConfigs' })} : `}
+          </Typography>
+          <Box>
+            <Typography component="span">
+              {formatMessage({
+                id:
+                  classrooms.length === 0
+                    ? 'noAvailableClassrooms'
+                    : selectedClassroomCodes.length === classrooms.length
+                    ? 'reuseAllClassrooms'
+                    : selectedClassroomCodes.length === 0
+                    ? 'noClassroom'
+                    : 'excludedClasses',
+              })}
+            </Typography>
+            {!(
+              selectedClassroomCodes.length === classrooms.length ||
+              selectedClassroomCodes.length === 0
+            ) && (
+              <>
+                {' '}
+                <Box component="span">
+                  {excludedClassrooms.map(
+                    ({ major_name, classroomLevels }, index) => (
+                      <>
+                        <Typography
+                          component={'span'}
+                          key={index}
+                        >{`${major_name} - ${formatMessage({
+                          id: `level${classroomLevels.length > 1 ? 's' : ''}`,
+                        })}( ${classroomLevels} )`}</Typography>{' '}
+                      </>
+                    )
+                  )}
+                </Box>
+              </>
+            )}
+          </Box>
+        </Box>
+      )}
+    </>
   );
 }
