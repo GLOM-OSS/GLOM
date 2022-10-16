dc5ede43c1d955ff879226ceb1fdf24e39ea0b7d
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-10-16 06:54:43 +0100

    added display section when not active

--- apps/app/src/components/secretary/newAcademicYear/coordinators.tsx
+++ apps/app/src/components/secretary/newAcademicYear/coordinators.tsx
@@ -1,40 +1,65 @@
 import {
-    FormControl,
-    FormControlLabel, Radio,
-    RadioGroup
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Typography,
 } from '@mui/material';
 import { Box } from '@mui/system';
+import { theme } from '@squoolr/theme';
 import { useIntl } from 'react-intl';
 
 export default function CoordinatorsConfig({
   reuseCoordinatorsConfig,
   setReuseCoordinatorsConfig,
+  isActive,
 }: {
   reuseCoordinatorsConfig: boolean;
   setReuseCoordinatorsConfig: (val: boolean) => void;
+  isActive: boolean;
 }) {
   const { formatMessage } = useIntl();
   return (
     <Box>
-      <FormControl>
-        <RadioGroup
-          value={reuseCoordinatorsConfig}
-          onChange={(event) =>
-            setReuseCoordinatorsConfig(event.target.value === 'true')
-          }
+      {isActive && (
+        <FormControl>
+          <RadioGroup
+            value={reuseCoordinatorsConfig}
+            onChange={(event) =>
+              setReuseCoordinatorsConfig(event.target.value === 'true')
+            }
+          >
+            <FormControlLabel
+              value={false}
+              control={<Radio />}
+              label={formatMessage({ id: 'resetConfig' })}
+            />
+            <FormControlLabel
+              value={true}
+              control={<Radio />}
+              label={formatMessage({ id: 'reuseTemplate' })}
+            />
+          </RadioGroup>
+        </FormControl>
+      )}
+      {!isActive && (
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'auto 1fr',
+            gap: theme.spacing(1),
+          }}
         >
-          <FormControlLabel
-            value={false}
-            control={<Radio />}
-            label={formatMessage({ id: 'resetConfig' })}
-          />
-          <FormControlLabel
-            value={true}
-            control={<Radio />}
-            label={formatMessage({ id: 'reuseTemplate' })}
-          />
-        </RadioGroup>
-      </FormControl>
+          <Typography sx={{ color: theme.common.placeholder }}>
+            {`${formatMessage({ id: 'academicServiceConfigs' })} : `}
+          </Typography>
+          <Typography>
+            {formatMessage({
+              id: reuseCoordinatorsConfig ? 'reuseTemplate' : 'resetConfig',
+            })}
+          </Typography>
+        </Box>
+      )}
     </Box>
   );
 }
