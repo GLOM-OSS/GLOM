9a87c59cf5ca4513bf8f8d7ea39e688a4d40a911
Marco Kuidja <kuidjamarco@gmail.com>
2022-10-30 19:48:48 +0100

    preparing personnel configurators module APIs for integrations

---
+++ libs/api-services/src/lib/interfaces/index.ts
@@ -0,0 +1,8 @@
+export interface Person {
+  fisrt_name: string;
+  last_name: string;
+  email: string;
+  birthdate: Date;
+  gender: 'Male' | 'Female';
+  national_id_number: string;
+}
---
+++ libs/api-services/src/lib/personnel/configurators/index.ts
@@ -0,0 +1,25 @@
+import { http } from '@squoolr/axios';
+import { Person } from '../../interfaces';
+
+export async function getConfigurators(params: {
+  keywords?: string;
+  is_deleted?: boolean;
+}) {
+  const {
+    data: { personnel },
+  } = await http.get(`/personnel/configurators/all`, {
+    params,
+  });
+  return personnel;
+}
+
+export async function getConfigurator(annual_configurator_id: string) {
+  const {
+    data: { personnel },
+  } = await http.get(`/personnel/configurators/${annual_configurator_id}`);
+  return personnel;
+}
+
+export async function addNewConfigurator(newConfigurator: Person) {
+  await http.post(`/personnel/configurators/new`, newConfigurator);
+}
