0b3cfba805ac74064f597b051a82fbff3a9467d3
Marco Kuidja <kuidjamarco@gmail.com>
2022-11-01 18:57:54 +0100

    prepraring seeded data API methodes

--- libs/api-services/src/lib/api.service.ts
+++ libs/api-services/src/lib/api.service.ts
@@ -1,6 +1,32 @@
-export * from './auth';
-export * from './demand'
+import { http } from '@squoolr/axios';
 
+export * from './auth';
+export * from './demand';
+export * from './departments';
+export * from './majors';
+export * from './classrooms';
+export * from './personnel';
+
+export async function getCycles() {
+  const {
+    data: { cycles },
+  } = await http.get(`/cycles`);
+  return cycles;
+}
+
+export async function getTeacherTypes() {
+  const {
+    data: { teacher_types },
+  } = await http.get(`/teacher-types`);
+  return teacher_types;
+}
+
+export async function getTeacherGrades() {
+  const {
+    data: { teaccher_grades },
+  } = await http.get(`/teacher-grades`);
+  return teaccher_grades;
+}
 export function apiServices(): string {
   return 'api-services';
 }
