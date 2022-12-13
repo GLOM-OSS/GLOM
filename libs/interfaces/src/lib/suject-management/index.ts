5f56f1f554f08523c0625543aa6009c02020c589
Marco Kuidja <kuidjamarco@gmail.com>
2022-12-13 09:20:02 +0100

    updated credit-unit interfaces

--- libs/interfaces/src/lib/suject-management/index.ts
+++ libs/interfaces/src/lib/suject-management/index.ts
@@ -1,43 +1,13 @@
-export interface CreditUnit {
-  annual_credit_unit_id: string;
-  academic_year_id: string;
-  major_id: string;
-  credit_unit_code: string;
-  credit_unit_name: string;
-  credit_points: number;
-  semester_number: number;
-}
-
-export interface CreditUnit {
-  annual_credit_unit_id: string;
-  academic_year_id: string;
-  major_id: string;
-  credit_unit_code: string;
-  credit_unit_name: string;
-  credit_points: number;
-  semester_number: number;
-}
-
-export interface CreditUnitSubject {
-  annual_credit_unit_has_subject_id: number;
-  has_objective: boolean;
-  suject_code: string;
-  subject_id: string;
-  subject_title: string;
-  weighting: number;
-  subjectParts: {
-    subject_part_id: string;
-    subject_part_name: string;
-    number_of_hours: number;
-  }[];
-}
-
 export interface CreateCreditUnit {
   major_id: string;
-  credit_points: number;
   credit_unit_code: string;
   credit_unit_name: string;
+  credit_points: number;
   semester_number: number;
+}
+
+export interface CreditUnit extends CreateCreditUnit {
+  annual_credit_unit_id: string;
 }
 
 export interface CreateCreditUnitSubject {
@@ -51,4 +21,8 @@ export interface CreateCreditUnitSubject {
     subject_part_id: string;
     number_of_hours: number;
   }[];
+}
+
+export interface CreditUnitSubject extends CreateCreditUnitSubject {
+  annual_credit_unit_has_subject_id: number;
 }
