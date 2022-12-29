d7a7eae3489eff55d099933f3e083875c3cbee25
Marco Kuidja <kuidjamarco@gmail.com>
2022-12-29 12:20:05 +0100

    Added mark traking interfaces to evaluation management interfaces

--- libs/interfaces/src/lib/evaluation/index.ts
+++ libs/interfaces/src/lib/evaluation/index.ts
@@ -19,15 +19,29 @@ export interface AnonimatedEvaluationHasStudent
 }
 
 export interface EvaluationSubType {
-    evaluation_sub_type_id: string;
-    evaluation_sub_type_name: string;
+  evaluation_sub_type_id: string;
+  evaluation_sub_type_name: string;
 }
 
 export interface Evaluation {
-    evaluation_id: string;
-    subject_name: string;
-    is_published: boolean;
-    is_anonimated: boolean;
-    examination_date: boolean;
-    evaluation_sub_type_name: string;
+  evaluation_id: string;
+  subject_name: string;
+  is_published: boolean;
+  is_anonimated: boolean;
+  examination_date: boolean;
+  evaluation_sub_type_name: string;
+}
+
+export interface UEMarkTraking {
+  credit_points: number;
+  credit_unit_code: string;
+  credit_unit_name: string;
+  availability_percentage: number;
+}
+
+export interface UVMarkTraking
+  extends Omit<Course, 'is_planified' | 'classroom_acronym'> {
+  is_ca_available: boolean;
+  is_exam_available: boolean;
+  is_receit_available: boolean;
 }
