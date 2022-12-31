90f79b50e1569b7ece9c2e589c41aab8edce2fc4
Marco Kuidja <kuidjamarco@gmail.com>
2022-12-31 15:55:21 +0100

    added course interfaces and update database and prisma schema in respect to course management module

---
+++ libs/interfaces/src/lib/course/index.ts
@@ -0,0 +1,22 @@
+export interface CreateLink {
+    resource_ref: string;
+    resource_name: string;
+}
+
+export interface Resource extends CreateLink {
+  resource_id: string;
+  chaper_id: string | null;
+  resource_type: 'FILE' | 'LINK';
+  resource_extension: string | null;
+  annual_credit_unit_subject_id: string;
+}
+
+export interface CreateChapter {
+  chapter_title: string;
+  chapter_objective: string;
+}
+
+export interface Chapter extends CreateChapter {
+  chapter_id: string;
+  chapter_parent_id: string;
+}
