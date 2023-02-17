08827a21384c4d244baea4932e831b3aba37bcb5
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-02-17 22:19:54 +0100

    added chapter and student select functionality loaded data of undoneChapters and subjectStudents

--- apps/app/src/components/teacher/presence/sessionDetails.tsx
+++ apps/app/src/components/teacher/presence/sessionDetails.tsx
@@ -1,10 +1,11 @@
-import { ExpandMoreOutlined } from '@mui/icons-material';
+import { ExpandMoreOutlined, ReportRounded } from '@mui/icons-material';
 import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
   Box,
   Button,
+  Checkbox,
   lighten,
   Table,
   TableBody,
@@ -13,10 +14,19 @@ import {
   TableRow,
   Typography,
 } from '@mui/material';
-import { PresenceList } from '@squoolr/interfaces';
+import { TimePicker } from '@mui/x-date-pickers';
+import {
+  Chapter,
+  PresenceList,
+  PresenceListChapter,
+  Student,
+} from '@squoolr/interfaces';
 import { theme } from '@squoolr/theme';
+import { ErrorMessage, useNotification } from '@squoolr/toast';
+import { useEffect, useState } from 'react';
 import Scrollbars from 'react-custom-scrollbars-2';
 import { useIntl } from 'react-intl';
+import { useParams } from 'react-router';
 import { NoTableElement } from '../courseLane';
 import PresenceStudentLane from './presenceStudentLane';
 
@@ -33,6 +43,149 @@ export default function SessionDetails({
   session: PresenceList;
 }) {
   const { formatMessage, formatDate, formatTime } = useIntl();
+  const { annual_credit_unit_subject_id } = useParams();
+
+  const [areNotDoneChaptersLoading, setAreNotDoneChaptersLoading] =
+    useState<boolean>(false);
+  const [notDoneChapters, setNotDoneChapters] = useState<PresenceListChapter[]>(
+    []
+  );
+  const [notDoneChapterNotif, setNotDoneChapterNotif] =
+    useState<useNotification>();
+
+  const loadNotDoneChapters = (annual_credit_unit_subject_id: string) => {
+    setAreNotDoneChaptersLoading(true);
+    const notif = new useNotification();
+    if (notDoneChapterNotif) {
+      notDoneChapterNotif.dismiss();
+    }
+    setNotDoneChapterNotif(notif);
+    setTimeout(() => {
+      //TODO: CALL API HERE TO LOAD subject's notDoneChapters with data annual_credit_unit_subject_id
+      // eslint-disable-next-line no-constant-condition
+      if (5 > 4) {
+        const newChapters: PresenceListChapter[] = [];
+        setNotDoneChapters(newChapters);
+        setAreNotDoneChaptersLoading(false);
+        notif.dismiss();
+        setNotDoneChapterNotif(undefined);
+      } else {
+        notif.notify({
+          render: formatMessage({ id: 'loadingNotDoneChapters' }),
+        });
+        notif.update({
+          type: 'ERROR',
+          render: (
+            <ErrorMessage
+              retryFunction={() =>
+                loadNotDoneChapters(annual_credit_unit_subject_id)
+              }
+              notification={notif}
+              //TODO: message should come from backend
+              message={formatMessage({ id: 'getNotDoneChaptersFailed' })}
+            />
+          ),
+          autoClose: false,
+          icon: () => <ReportRounded fontSize="medium" color="error" />,
+        });
+      }
+    }, 3000);
+  };
+
+  const [areSubjectStudentsLoading, setAreSubjectStudentsLoading] =
+    useState<boolean>(false);
+  const [subjectStudents, setSubjectStudents] = useState<Student[]>([]);
+  const [subjectStudentNotif, setSubjectStudentNotif] =
+    useState<useNotification>();
+
+  const loadSubjectStudents = (annual_credit_unit_subject_id: string) => {
+    setAreSubjectStudentsLoading(true);
+    const notif = new useNotification();
+    if (subjectStudentNotif) {
+      subjectStudentNotif.dismiss();
+    }
+    setSubjectStudentNotif(notif);
+    setTimeout(() => {
+      //TODO: CALL API HERE TO LOAD student's offering subject (subjectStudents) with data annual_credit_unit_subject_id
+      // eslint-disable-next-line no-constant-condition
+      if (5 > 4) {
+        const newStudents: Student[] = [];
+        setSubjectStudents(newStudents);
+        setAreSubjectStudentsLoading(false);
+        notif.dismiss();
+        setSubjectStudentNotif(undefined);
+      } else {
+        notif.notify({
+          render: formatMessage({ id: 'loadingStudents' }),
+        });
+        notif.update({
+          type: 'ERROR',
+          render: (
+            <ErrorMessage
+              retryFunction={() =>
+                loadSubjectStudents(annual_credit_unit_subject_id)
+              }
+              notification={notif}
+              //TODO: message should come from backend
+              message={formatMessage({ id: 'getSubjectStudentsFailed' })}
+            />
+          ),
+          autoClose: false,
+          icon: () => <ReportRounded fontSize="medium" color="error" />,
+        });
+      }
+    }, 3000);
+  };
+
+  useEffect(() => {
+    if (!ip) {
+      loadSubjectStudents(annual_credit_unit_subject_id as string);
+      loadNotDoneChapters(annual_credit_unit_subject_id as string);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [addedChapterIds, setAddedChapterIds] = useState<string[]>([]);
+  const [addedStudentIds, setAddedStudentIds] = useState<string[]>([]);
+  const [removedChapterIds, setRemovedChapterIds] = useState<string[]>([]);
+  const [removedStudentIds, setRemovedStudentIds] = useState<string[]>([]);
+
+  const CL = c.map(({ chapter_id: c_id }) => c_id);
+  const selectChapter = (chapter: PresenceListChapter) => {
+    if (CL.includes(chapter.chapter_id)) {
+      if (removedChapterIds.includes(chapter.chapter_id)) {
+        setRemovedChapterIds(
+          removedChapterIds.filter((c_id) => c_id !== chapter.chapter_id)
+        );
+      } else setRemovedChapterIds([...removedChapterIds, chapter.chapter_id]);
+    } else {
+      if (addedChapterIds.includes(chapter.chapter_id))
+        setAddedChapterIds(
+          addedChapterIds.filter((c_id) => c_id !== chapter.chapter_id)
+        );
+      else setAddedChapterIds([...addedChapterIds, chapter.chapter_id]);
+    }
+  };
+
+  const SL = s.map(({ annual_student_id: as_id }) => as_id);
+  const selectStudent = (student: Student) => {
+    if (SL.includes(student.annual_student_id)) {
+      if (removedStudentIds.includes(student.annual_student_id)) {
+        setRemovedStudentIds(
+          removedStudentIds.filter(
+            (as_id) => as_id !== student.annual_student_id
+          )
+        );
+      } else
+        setRemovedStudentIds([...removedStudentIds, student.annual_student_id]);
+    } else {
+      if (addedStudentIds.includes(student.annual_student_id))
+        setAddedStudentIds(
+          addedStudentIds.filter((as_id) => as_id !== student.annual_student_id)
+        );
+      else setAddedStudentIds([...addedStudentIds, student.annual_student_id]);
+    }
+  };
 
   return (
     <Box
@@ -102,7 +255,13 @@ export default function SessionDetails({
                   </Typography>
                 </Box>
               ) : (
-                'input fields'
+                'adi cam solve am small time'
+                // <TimePicker
+                //   label={formatMessage({ id: 'startTime' })}
+                //   value={}
+                //   onChange={handleChange}
+                //   renderInput={(params) => <TextField {...params} />}
+                // />
               )}
             </Box>
             <Box sx={{ display: 'grid', rowGap: 1 }}>
@@ -138,7 +297,31 @@ export default function SessionDetails({
                     })
                   )
                 ) : (
-                  'make it rain'
+                  [...notDoneChapters, ...c].map((chapter, index) => (
+                    <Box
+                      sx={{
+                        display: 'grid',
+                        gridTemplateColumns: 'auto 1fr',
+                        columnGap: 0.5,
+                        alignItems: 'center',
+                        cursor: 'pointer',
+                      }}
+                      onClick={() => selectChapter(chapter)}
+                      key={index}
+                    >
+                      <Checkbox
+                        checked={
+                          (CL.includes(chapter.chapter_id) &&
+                            !removedChapterIds.includes(chapter.chapter_id)) ||
+                          (!CL.includes(chapter.chapter_id) &&
+                            addedChapterIds.includes(chapter.chapter_id))
+                        }
+                      />
+                      <Typography component="span">
+                        {chapter.chapter_title}
+                      </Typography>
+                    </Box>
+                  ))
                 )}
               </Box>
             </Box>
@@ -201,7 +384,20 @@ export default function SessionDetails({
                     : -1
                 )
                 .map((student, index) => (
-                  <PresenceStudentLane student={student} key={index} />
+                  <PresenceStudentLane
+                    student={student}
+                    key={index}
+                    is_published={ip}
+                    onSelect={() => selectStudent(student)}
+                    isSelected={
+                      (SL.includes(student.annual_student_id) &&
+                        !removedStudentIds.includes(
+                          student.annual_student_id
+                        )) ||
+                      (!SL.includes(student.annual_student_id) &&
+                        addedStudentIds.includes(student.annual_student_id))
+                    }
+                  />
                 ))
             )}
           </TableBody>
