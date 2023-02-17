04690c0652e0c27f441a647ce5d95ed883e76b66
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-02-17 22:17:59 +0100

    made the student list display when it's not published and added slect functionality

--- apps/app/src/components/teacher/presence/presenceStudentLane.tsx
+++ apps/app/src/components/teacher/presence/presenceStudentLane.tsx
@@ -1,11 +1,17 @@
-import { TableCell, TableRow } from '@mui/material';
+import { Checkbox, TableCell, TableRow } from '@mui/material';
 import { Student } from '@squoolr/interfaces';
 import { theme } from '@squoolr/theme';
 
 export default function PresenceStudentLane({
   student: { matricule: m, first_name: fn, last_name: ln },
+  onSelect,
+  isSelected,
+  is_published: ip,
 }: {
   student: Student;
+  isSelected: boolean;
+  onSelect: () => void;
+  is_published: boolean;
 }) {
   return (
     <TableRow
@@ -18,7 +24,11 @@ export default function PresenceStudentLane({
     >
       <TableCell>{m}</TableCell>
       <TableCell>{`${fn} ${ln}`}</TableCell>
-      {/* <TableCell></TableCell> */}
+      {!ip && (
+        <TableCell align="right">
+          <Checkbox onChange={onSelect} checked={isSelected} />
+        </TableCell>
+      )}
     </TableRow>
   );
 }
