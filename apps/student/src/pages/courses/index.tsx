c646958ec06a75bdd0fd08bb1aaf763820395a03
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-02-21 09:33:23 +0100

    added student courses page

---
+++ apps/student/src/pages/courses/index.tsx
@@ -0,0 +1,180 @@
+import { ReportRounded } from '@mui/icons-material';
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  lighten,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import { Course } from '@squoolr/interfaces';
+import { useUser } from '@squoolr/layout';
+import { theme } from '@squoolr/theme';
+import { ErrorMessage, useNotification } from '@squoolr/toast';
+import { useEffect, useState } from 'react';
+import Scrollbars from 'react-custom-scrollbars-2';
+import { useIntl } from 'react-intl';
+import CourseLane from '../../components/course/courseLane';
+import {
+  NoTableElement,
+  TableLaneSkeleton,
+} from '../../components/helpers/tables';
+
+export default function Courses() {
+  const { formatMessage } = useIntl();
+  const { student } = useUser();
+
+  const [areCoursesLoading, setAreCoursesLoading] = useState<boolean>(false);
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [courseNotif, setCourseNotif] = useState<useNotification>();
+
+  const loadCourses = (activeSemester?: number) => {
+    setAreCoursesLoading(true);
+    const notif = new useNotification();
+    if (courseNotif) {
+      courseNotif.dismiss();
+    }
+    setCourseNotif(notif);
+    setTimeout(() => {
+      //TODO: CALL API HERE TO LOAD student's courses with data activeSemester
+      // eslint-disable-next-line no-constant-condition
+      if (5 > 4) {
+        const newCourses: Course[] = [
+          {
+            annual_credit_unit_subject_id: 'sieo',
+            classroomAcronyms: [],
+            has_course_plan: false,
+            is_ca_available: false,
+            is_exam_available: false,
+            is_resit_available: false,
+            objective: 'Make it rain',
+            subject_code: 'UC0116',
+            subject_title: "Introduction a l'algorithmique",
+            semester: 2,
+          },
+        ];
+        setCourses(newCourses);
+        setAreCoursesLoading(false);
+        notif.dismiss();
+        setCourseNotif(undefined);
+      } else {
+        notif.notify({
+          render: formatMessage({ id: 'loadingCourses' }),
+        });
+        notif.update({
+          type: 'ERROR',
+          render: (
+            <ErrorMessage
+              retryFunction={() => loadCourses(activeSemester)}
+              notification={notif}
+              //TODO: message should come from backend
+              message={formatMessage({ id: 'getCoursesFailed' })}
+            />
+          ),
+          autoClose: false,
+          icon: () => <ReportRounded fontSize="medium" color="error" />,
+        });
+      }
+    }, 3000);
+  };
+
+  const [activeSemester, setActiveSemester] = useState<number>(0);
+
+  useEffect(() => {
+    loadCourses(activeSemester);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeSemester]);
+
+  return (
+    <Box
+      sx={{
+        height: '100%',
+        display: 'grid',
+        gridTemplateRows: 'auto 1fr',
+        rowGap: 2,
+      }}
+    >
+      <FormControl sx={{ marginTop: 1, justifySelf: 'start' }}>
+        <InputLabel id="semester">
+          {formatMessage({ id: 'semester' })}
+        </InputLabel>
+        <Select
+          labelId="semester"
+          disabled={!student || areCoursesLoading}
+          value={activeSemester}
+          size="small"
+          onChange={(event) => setActiveSemester(Number(event.target.value))}
+          sx={{ minWidth: '100px' }}
+          input={<OutlinedInput label={formatMessage({ id: 'semester' })} />}
+          MenuProps={{
+            PaperProps: {
+              style: {
+                maxHeight: 48 * 4.5 + 8,
+              },
+            },
+          }}
+        >
+          <MenuItem value={undefined}>{formatMessage({ id: 'all' })}</MenuItem>
+          {[...new Array(student ? student.classroom_level * 2 : 0)].map(
+            (_, index) => (
+              <MenuItem key={index} value={index + 1}>
+                {index + 1}
+              </MenuItem>
+            )
+          )}
+        </Select>
+      </FormControl>
+      <Scrollbars autoHide>
+        <Table sx={{ minWidth: 650 }}>
+          <TableHead
+            sx={{
+              backgroundColor: lighten(theme.palette.primary.light, 0.6),
+            }}
+          >
+            <TableRow>
+              {['subjectCode', 'subjectTitle', 'semester'].map((val, index) => (
+                <TableCell key={index} align={index === 2 ? 'right' : 'left'}>
+                  {formatMessage({ id: val })}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {areCoursesLoading ? (
+              [...new Array(5)].map((_, index) => (
+                <TableLaneSkeleton cols={3} key={index} />
+              ))
+            ) : courses.length === 0 ? (
+              <NoTableElement
+                message={formatMessage({
+                  id: 'noRegisteredCourse',
+                })}
+                colSpan={3}
+              />
+            ) : (
+              courses
+                .sort((a, b) =>
+                  (a.semester as number) > (b.semester as number)
+                    ? 1
+                    : a.subject_title > b.subject_title
+                    ? 1
+                    : a.subject_code > b.subject_code
+                    ? 1
+                    : -1
+                )
+                .map((course, index) => (
+                  <CourseLane course={course} key={index} />
+                ))
+            )}
+          </TableBody>
+        </Table>
+      </Scrollbars>
+    </Box>
+  );
+}
