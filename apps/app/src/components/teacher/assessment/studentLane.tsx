21a94183d2c9b955289a23b331324c56f02a9378
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-02-24 20:13:58 +0100

    added group lane component

--- apps/app/src/components/teacher/assessment/studentLane.tsx
+++ apps/app/src/components/teacher/assessment/studentLane.tsx
@@ -1,17 +1,17 @@
 import { Chip, TableCell, TableRow } from '@mui/material';
-import { StudentAssessmentAnswer } from '@squoolr/interfaces';
+import { IGroupAssignment, StudentAssessmentAnswer } from '@squoolr/interfaces';
 import { useIntl } from 'react-intl';
 
 export default function StudentLane({
   position: p,
   onSelect,
   student: { fullname, matricule, submitted_at, total_score },
-  total
+  total,
 }: {
   position: number;
   student: Omit<StudentAssessmentAnswer, 'questionAnswers'>;
   onSelect: () => void;
-  total:number
+  total: number;
 }) {
   const { formatDate, formatNumber } = useIntl();
   return (
@@ -20,7 +20,10 @@ export default function StudentLane({
       <TableCell>{matricule}</TableCell>
       <TableCell>{fullname}</TableCell>
       <TableCell>
-        <Chip label={`${formatNumber(total_score)} / ${total}`} variant="outlined" />
+        <Chip
+          label={`${formatNumber(total_score)} / ${total}`}
+          variant="outlined"
+        />
       </TableCell>
       <TableCell>
         {formatDate(new Date(submitted_at), {
@@ -31,6 +34,36 @@ export default function StudentLane({
           minute: '2-digit',
           timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
         })}
+      </TableCell>
+    </TableRow>
+  );
+}
+
+export function GroupLane({
+  position: p,
+  onSelect,
+  group: { group_code, total_score, number_of_students, is_submitted },
+  total,
+}: {
+  position: number;
+  group: IGroupAssignment;
+  onSelect: () => void;
+  total: number;
+}) {
+  const { formatNumber, formatMessage } = useIntl();
+  return (
+    <TableRow hover onClick={() => onSelect()}>
+      <TableCell>{p}</TableCell>
+      <TableCell>{group_code}</TableCell>
+      <TableCell>{formatNumber(number_of_students)}</TableCell>
+      <TableCell>
+        <Chip
+          label={`${formatNumber(total_score)} / ${total}`}
+          variant="outlined"
+        />
+      </TableCell>
+      <TableCell>
+        {formatMessage({ id: is_submitted ? 'submitted' : 'notSubmitted' })}
       </TableCell>
     </TableRow>
   );
