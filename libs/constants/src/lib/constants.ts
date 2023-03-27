9444411ef88abae64f92d163ca65890ea8fc0a5d
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-27 16:34:18 +0100

    changed base url

--- libs/constants/src/lib/constants.ts
+++ libs/constants/src/lib/constants.ts
@@ -1,8 +1,7 @@
 export const constants = {
-  // ? 'https://api.squoolr.com'
   NX_API_BASE_URL:
     process.env['NODE_ENV'] === 'production'
-      ? 'https://localhost:83'
+      ? 'https://api.squoolr.com'
       : 'http://localhost:8000',
   NX_CRYPTJS_SECRET: 'I_hate_trash',
 };
