4c678fedf1210c9588b14216526411f24e695c1c
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-29 12:52:33 +0100

    added guard conditions and toast error messages to open evaluation added confirm dialog to open evaluation

--- apps/student/src/components/assessment/index.tsx
+++ apps/student/src/components/assessment/index.tsx
@@ -1,9 +1,12 @@
+import { takeAssessment } from '@squoolr/api-services';
+import { ConfirmDeleteDialog } from '@squoolr/dialogTransition';
 import {
   Assessment,
   IGroupAssignment,
   StudentAssessmentAnswer,
 } from '@squoolr/interfaces';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 import AssessmentList from './assessmentList';
 import QuestionList from './questionList';
 
@@ -13,17 +16,62 @@ export type SubmissionEntity =
 
 export default function Assessments() {
   const [activeAssessment, setActiveAssessment] = useState<Assessment>();
+  const [
+    isConfirmStartAssessmentDialogOpen,
+    setIsConfirmStartAssessementDialogOpen,
+  ] = useState<boolean>(false);
 
-  return !activeAssessment ? (
-    <AssessmentList
-      createAssessment={() => alert('hello')}
-      isCreatingAssessment={false}
-      setActiveAssessment={(val: Assessment) => setActiveAssessment(val)}
-    />
-  ) : (
-    <QuestionList
-      activeAssessment={activeAssessment}
-      setActiveAssessment={setActiveAssessment}
-    />
+  const [pendingConfirm, setPendingConfirm] = useState<Assessment>();
+
+  const takeAssessmentHandler = (assessment: Assessment) => {
+    takeAssessment(assessment.assessment_id)
+      .then(() => setActiveAssessment(assessment))
+      .catch((error) => toast.error(error.message));
+  };
+
+  return (
+    <>
+      <ConfirmDeleteDialog
+        closeDialog={() => {
+          setIsConfirmStartAssessementDialogOpen(false);
+          setPendingConfirm(undefined);
+        }}
+        confirm={() => {
+          takeAssessmentHandler(pendingConfirm as Assessment);
+          setActiveAssessment(pendingConfirm);
+          setPendingConfirm(undefined);
+        }}
+        dialogMessage={'confirmStartAssessmentMessage'}
+        isDialogOpen={isConfirmStartAssessmentDialogOpen}
+        confirmButton={'startAssessment'}
+        danger
+        dialogTitle="confirmStartAssessment"
+      />
+      {!activeAssessment ? (
+        <AssessmentList
+          isCreatingAssessment={false}
+          createAssessment={() => toast.error('Not allow')}
+          setActiveAssessment={(val: Assessment) => {
+            const { assessment_date, duration } = val;
+            if (assessment_date && duration) {
+              const rejectDuration = new Date(
+                new Date(assessment_date).getTime() + (duration / 8) * 60 * 1000
+              );
+              if (new Date() > rejectDuration)
+                toast.error('You are late for this evaluation');
+              else {
+                setIsConfirmStartAssessementDialogOpen(true);
+                setPendingConfirm(val);
+              }
+            } else toast.error('Evaluation not activated yet');
+          }}
+        />
+      ) : (
+        <QuestionList
+          activeAssessment={activeAssessment}
+          setActiveAssessment={setActiveAssessment}
+        />
+      )}
+    </>
   );
 }
