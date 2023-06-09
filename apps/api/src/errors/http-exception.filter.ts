ce86921a161978ac89b2077dc145666bf6d33984
Marco Kuidja <kuidjamarco@gmail.com>
2023-06-09 14:05:38 +0100

    updated http errors handling prisma errors

--- apps/api/src/errors/http-exception.filter.ts
+++ apps/api/src/errors/http-exception.filter.ts
@@ -5,6 +5,7 @@ import {
   HttpException,
   HttpStatus,
   BadRequestException,
+  Logger,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
@@ -16,13 +17,18 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest<Request>();
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const { statusCode, error, message } = exception.getResponse() as any;
-    let errorMessage: string;
+    let errorMessage: string = message ?? exception.message;
     try {
-      errorMessage = JSON.parse(message ?? exception.message)[
-        request.headers['lang'] as string || 'fr'
-      ];
+      errorMessage =
+        JSON.parse(errorMessage)[(request.headers['lang'] as string) || 'fr'];
     } catch (error) {
-      errorMessage = message ?? exception.message;
+      Logger.error(errorMessage, request.url);
+      errorMessage = errorMessage.includes('prisma')
+        ? `Sorry, we couldn't persist your data. Contact your developer with this error: ${errorMessage.slice(
+            errorMessage.indexOf('.') + 1,
+            errorMessage.indexOf('()')
+          )} received invalid args.`
+        : errorMessage;
     }
     response.status(statusCode ?? HttpStatus.INTERNAL_SERVER_ERROR).json({
       error,
