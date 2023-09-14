4afc38666c80eed954e126572a2ec1ee15268536
Marco Kuidja <kuidjamarco@gmail.com>
2023-09-14 21:37:21 +0100

    feat(shared): #11 updated auth module providers

--- libs/nest/auth/src/lib/glom-auth.module.ts
+++ libs/nest/auth/src/lib/glom-auth.module.ts
@@ -1,15 +1,17 @@
 import { GlomPrismaModule } from '@glom/prisma';
 import { Module } from '@nestjs/common';
 import { GlomAuthService } from './glom-auth.service';
+import { GlomMailerModule } from '@glom/nest-mailer';
+import { LocalStrategy } from './local/local.strategy';
 
 @Module({})
 export class GlomAuthModule {
-  forRoot() {
+  static forRoot() {
     return {
       module: GlomAuthModule,
       exports: [GlomAuthService],
-      providers: [GlomAuthService],
-      imports: [GlomPrismaModule.forRoot({})],
+      providers: [GlomAuthService, LocalStrategy],
+      imports: [GlomPrismaModule.forRoot({}), GlomMailerModule],
     };
   }
 }
