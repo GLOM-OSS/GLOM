6814f495898c6f0b0dce54461e042a9953e49d08
Marco Kuidja <kuidjamarco@gmail.com>
2023-09-14 15:44:42 +0100

    fix(shared): #13 removed unsed templates messages imported from `xafpay`

--- libs/nest/mailer/src/index.ts
+++ libs/nest/mailer/src/index.ts
@@ -1,8 +1,6 @@
 export * from './lib/mailer.module';
 export * from './lib/mailer.service';
 export * from './lib/messages/created-account.messages';
-export * from './lib/messages/requires-update.messages';
 export * from './lib/messages/reset-pwrd.messages';
-export * from './lib/messages/transfer-mail.messages';
 export * from './lib/messages/utils';
 
