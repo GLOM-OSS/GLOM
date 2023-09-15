6e2850418a07b2c2e4a675cb21289a936d7e717d
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-09-15 01:13:38 +0100

    feat(app): adjusted base file for lynkr landing

--- apps/lynkr-landing/pages/index.tsx
+++ apps/lynkr-landing/pages/index.tsx
@@ -1,23 +1,7 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 export function Index() {
-  return (
-    <Box sx={{ height: '100%' }}>
-      <Box
-      // sx={{
-      //   display: 'grid',
-      //   gridTemplateRows: 'auto auto 1fr',
-      //   rowGap: '30px',
-      //   height: '100%',
-      //   padding: '0 16px',
-      // }}
-      >
-        <Typography className="h1--mobile">Lynkr</Typography>
-        <Typography className="p1--space">Lynkr number 2</Typography>
-        <Button color='secondary' variant='contained'>Hello</Button>
-      </Box>
-    </Box>
-  );
+  return <Box sx={{ height: '100%', padding: '0 16px' }}></Box>;
 }
 
 export default Index;
