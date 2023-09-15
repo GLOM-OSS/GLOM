3de0badad4d276d49e4e93d5214d6d04ccd8800a
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-09-15 01:10:50 +0100

    feat(app): adjusted favicon change code

--- apps/lynkr-landing/pages/_app.tsx
+++ apps/lynkr-landing/pages/_app.tsx
@@ -1,31 +1,33 @@
+import { Box } from '@mui/material';
+import { GlomThemeProvider } from '@squoolr/theme';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { Box } from '@mui/material';
 import { useEffect, useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
-import '../public/styles/root.scss';
 import '../public/styles/global.scss';
 import '../public/styles/notifGlobalStyles.css';
 import '../public/styles/reset.css';
-import { GlomThemeProvider } from '@squoolr/theme';
+import '../public/styles/root.scss';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
 
+  function updateWindowMode() {
+    if (document.hidden) {
+      setIsDarkMode(
+        window.matchMedia &&
+          window.matchMedia('(prefers-color-scheme: dark)').matches
+      );
+    } else {
+      setIsDarkMode(
+        window.matchMedia &&
+          window.matchMedia('(prefers-color-scheme: dark)').matches
+      );
+    }
+  }
   useEffect(() => {
-    document.addEventListener('visibilitychange', function (event) {
-      if (document.hidden) {
-        setIsDarkMode(
-          window.matchMedia &&
-            window.matchMedia('(prefers-color-scheme: dark)').matches
-        );
-      } else {
-        setIsDarkMode(
-          window.matchMedia &&
-            window.matchMedia('(prefers-color-scheme: dark)').matches
-        );
-      }
-    });
+    updateWindowMode();
+    document.addEventListener('visibilitychange', updateWindowMode);
   }, []);
 
   return (
@@ -41,13 +43,12 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <main
         className="app"
         style={{
-          padding: '0 16px',
           height: '100vh',
           display: 'grid',
           gridAutoFlow: 'column',
         }}
       >
-        <Box sx={{ minWidth: { tablet: '48vw', mobile: '100vw' } }}>
+        <Box>
           <Component {...pageProps} />
         </Box>
       </main>
