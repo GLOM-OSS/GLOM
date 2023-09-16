c612458d10898ee7b1ecba7cb099f224288db696
Marco Kuidja <kuidjamarco@gmail.com>
2023-09-16 22:28:18 +0100

    fix(server): #17 added global filter to lynkr-api

--- apps/lynkr-api/src/main.ts
+++ apps/lynkr-api/src/main.ts
@@ -7,16 +7,15 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { GlomExceptionsFilter } from '@glom/execeptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  app.useGlobalFilters(new GlomExceptionsFilter());
+
   const port = process.env.APP_PORT || 3333;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
