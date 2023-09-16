05834a09c946a45d1f8c529babf8c4473dad2276
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-09-16 18:29:23 +0100

    feat(app): added navbar to index also tested sections and navigations

--- apps/lynkr-landing/pages/index.tsx
+++ apps/lynkr-landing/pages/index.tsx
@@ -1,7 +1,40 @@
-import { Box } from '@mui/material';
+import { AppBar, Box } from '@mui/material';
+import Navbar, { ElevationScroll } from '../components/Navbar';
+import { useTheme } from '@glom/theme';
 
 export function Index() {
-  return <Box sx={{ height: '100%', padding: '0 16px' }}></Box>;
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        height: '100%',
+        backgroundColor: theme.common.offWhite,
+      }}
+    >
+      <ElevationScroll>
+        <AppBar color="default">
+          <Navbar />
+        </AppBar>
+      </ElevationScroll>
+
+      <Box
+        component="section"
+        id="hello"
+        height="100vh"
+        sx={{ backgroundColor: 'orange' }}
+      >
+        Hello
+      </Box>
+      <Box
+        component="section"
+        id="features"
+        height="100vh"
+        sx={{ backgroundColor: 'green', py: 10.5 }}
+      >
+        Features
+      </Box>
+    </Box>
+  );
 }
 
 export default Index;
