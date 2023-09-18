e97b1af85036858a494dea2c6e97f5c881927bd9
Marco Kuidja <kuidjamarco@gmail.com>
2023-09-18 18:31:28 +0100

    feat(shared): #18 added `facebook` strategy module to glom auth

---
+++ libs/nest/auth/src/lib/third-parthies/facebook/facebook.controller.ts
@@ -0,0 +1,50 @@
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiExcludeEndpoint,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { UserEntity } from '../../glom-auth.dto';
+import { Request, Response } from 'express';
+import { ClsService } from 'nestjs-cls';
+import { FacebookGuard } from './facebook.guard';
+
+@Controller()
+@ApiTags('Facebook Authentication')
+@ApiUnauthorizedResponse({
+  description: 'Unauthorized request. incorrect email or password',
+})
+@ApiBadRequestResponse({
+  description:
+    'Bad request. This often happens when the request payload it not respected.',
+})
+@ApiInternalServerErrorResponse({
+  description: 'Internal server error. An unexpected exception was thrown',
+})
+export class FacebookController {
+  constructor(private clsService: ClsService) {}
+
+  @Get()
+  @UseGuards(FacebookGuard)
+  @ApiOperation({
+    summary:
+      'Facebook authentication. It redirects the provided callback on sucessfully authentication.',
+  })
+  async facebookAuth(@Req() req: Request) {
+    return req.user;
+  }
+
+  @Get('callback')
+  @UseGuards(FacebookGuard)
+  @ApiExcludeEndpoint()
+  @ApiResponse({ status: 201, type: UserEntity })
+  async facebookCallback(@Res() res: Response) {
+    const refererUrl = this.clsService.get()['refererUrl'];
+    return res.redirect(refererUrl);
+  }
+}
