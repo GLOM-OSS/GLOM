74a6e47954c3dbb0f5737c8511dac088954792fd
Marco Kuidja <kuidjamarco@gmail.com>
2023-09-18 18:30:53 +0100

    fix(server): #18 added google strategy module to glom auth

---
+++ libs/nest/auth/src/lib/third-parthies/google/google.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { GoogleController } from './google.controller';
+import { GoogleStrategy } from './google.strategy';
+
+@Module({
+  controllers: [GoogleController],
+  providers: [GoogleStrategy],
+})
+export class GoogleModule {}
