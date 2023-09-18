a8eaa3dcde38649451eb37c754310a42b07b0e26
Marco Kuidja <kuidjamarco@gmail.com>
2023-09-18 19:30:25 +0100

    docs(server): #3 added swagger document builder to lynkr api

--- apps/lynkr-api/src/main.ts
+++ apps/lynkr-api/src/main.ts
@@ -3,15 +3,30 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { GlomExceptionsFilter } from '@glom/execeptions';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlomExceptionsFilter());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    })
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Lynkr APIs')
+    .setDescription("Detailed description of Lynkr's APIs.")
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.APP_PORT || 3333;
   await app.listen(port);
