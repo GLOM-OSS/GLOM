8d4506584aaec2e5f8e558d38872ef781f55c869
Marco Kuidja <kuidjamarco@gmail.com>
2023-09-20 12:47:49 +0100

    fix(app): #20 renamed apps in lynkr dir project

--- apps/lynkr/api/jest.config.ts
+++ apps/lynkr/api/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'lynkr-lynkr-api',
+  displayName: 'lynkr-api',
   preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/apps/lynkr/lynkr-api',
+  coverageDirectory: '../../../coverage/apps/lynkr/api',
 };
