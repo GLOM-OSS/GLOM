49f50fabc44e1dafd1265309478a30c86969c400
Marco Kuidja <kuidjamarco@gmail.com>
2023-10-10 14:12:27 +0100

    fix(shared): #11 adjusted facebook strategy validation logic

--- libs/nest/auth/src/lib/third-parthies/facebook/facebook.strategy.ts
+++ libs/nest/auth/src/lib/third-parthies/facebook/facebook.strategy.ts
@@ -1,14 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
-import { Profile, Strategy } from 'passport-google-oauth20';
+import { Profile, Strategy } from 'passport-facebook';
 import { GlomAuthService } from '../../glom-auth.service';
 
 @Injectable()
 export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
   constructor(private authService: GlomAuthService) {
     super({
-      callbackURL: `${process.env.NX_API_BASE_URL}/auth/third-parthies/facebook/callback`,
+      callbackURL: `${process.env.NX_API_BASE_URL}/auth/facebook/callback`,
       clientSecret: process.env.FACEBOOK_SECRET,
       clientID: process.env.FACEBOOK_APP_ID,
       profileFields: ['emails', 'name'],
@@ -18,11 +18,10 @@ export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
   }
 
   async validate(request: Request, _a: string, _r: string, profile: Profile) {
-    const {
-      _json: { email_verified },
-    } = profile;
-    console.log(profile);
-    if (!email_verified) throw new UnauthorizedException();
-    return this.authService.authenticateUser(request, profile);
+    const { emails } = profile;
+    return this.authService.authenticateUser(request, {
+      ...profile,
+      username: emails[0].value,
+    });
   }
 }
