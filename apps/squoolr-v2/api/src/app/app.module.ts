d02895f6141617ed659b3fbff2547190f34c22c2
Marco Kuidja <kuidjamarco@gmail.com>
2023-10-11 04:08:00 +0100

    feat(server): #25 configured AppModule and added based modules

--- apps/squoolr-v2/api/src/app/app.module.ts
+++ apps/squoolr-v2/api/src/app/app.module.ts
@@ -1,11 +1,54 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { InjectRedis, Redis, RedisModule } from '@nestjs-modules/ioredis';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PassportModule } from '@nestjs/passport';
+import { APP_GUARD } from '@nestjs/core';
+
+import * as session from 'express-session';
+import RedisStore from 'connect-redis';
+import * as passport from 'passport';
+import { randomUUID } from 'crypto';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot(),
+    PassportModule.register({
+      session: true,
+    }),
+    RedisModule.forRoot({
+      config: {
+        url: process.env.REDIS_URL,
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        session({
+          name: process.env.SESSION_NAME,
+          store: new RedisStore({ client: this.redis }),
+          secret: process.env.SESSION_SECRET,
+          genid: () => randomUUID(),
+          saveUninitialized: false,
+          resave: false,
+          rolling: true,
+          cookie: {
+            maxAge: 60 * 60 * 1000, //60 minutes of inativity,
+          },
+        }),
+        passport.initialize(),
+        passport.session()
+      )
+      .forRoutes('*');
+  }
+}
