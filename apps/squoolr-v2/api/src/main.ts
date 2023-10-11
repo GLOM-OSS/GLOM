d72c512966b3a9ca253819e7f31fdd9274126187
Marco Kuidja <kuidjamarco@gmail.com>
2023-10-11 03:51:12 +0100

    feat(server): #25 added swagger support to squoolr v2 api

--- apps/squoolr-v2/api/src/main.ts
+++ apps/squoolr-v2/api/src/main.ts
@@ -5,18 +5,44 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import path = require('path');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(path.join(__dirname, './assets'));
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Squoolr APIs')
+      .setDescription('Detailed description of Squoolr internal APIs.')
+      .setVersion('1.0')
+      .addCookieAuth(process.env.SESSION_NAME)
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document, {
+      customfavIcon: '/squoolr_favicon.png',
+      customSiteTitle: 'Squoolr APIs docs',
+      customCss: `
+        img[alt="Swagger UI"] {  
+          content: url('/squoolr_logo.png') 
+        }
+        .swagger-ui .topbar {
+          background: #D9DBE9
+        }
+        .swagger-ui .opblock {
+          border: none
+        }
+      `,
+    });
+  }
+  const port = process.env.APP_PORT || 3333;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
