3936b04a2ea94bd5bd9a0aedb6b45db0debfd8b6
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-10-11 22:55:54 +0100

    (Refactor): #24 updated navbar padding on mobile

--- apps/squoolr-v2/squoolr-landing/components/navigation/Navbar.tsx
+++ apps/squoolr-v2/squoolr-landing/components/navigation/Navbar.tsx
@@ -134,7 +134,7 @@ export default function Navbar() {
               columnGap: 1,
               alignItems: 'center',
               padding: {
-                mobile: '16px',
+                mobile: '11px 16px',
                 desktop: '11px 118px',
               },
               backgroundColor: '#EDF2FB',
