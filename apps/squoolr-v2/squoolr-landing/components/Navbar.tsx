bbab3b26e1b27be1c76f9ef9dd5344cfc067bb61
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-10-11 02:34:09 +0100

    feat(app): #24 added desktop navbar,

---
+++ apps/squoolr-v2/squoolr-landing/components/Navbar.tsx
@@ -0,0 +1,261 @@
+import list from '@iconify/icons-fluent/list-28-filled';
+import { Icon } from '@iconify/react';
+import {
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  MenuItem,
+  Select,
+  Tooltip,
+  Typography,
+  capitalize,
+  useScrollTrigger,
+} from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ReactElement, cloneElement } from 'react';
+import { useIntl } from 'react-intl';
+import { useActiveLanguage, useDispatchLanguage, useTheme } from '@glom/theme';
+
+export function ElevationScroll({ children }: { children: ReactElement }) {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export const scrollToSection = (sectionId: string) => {
+  const section = document.querySelector(sectionId);
+  section?.scrollIntoView({ behavior: 'smooth' });
+};
+
+export function getBaseUrl(usage: string) {
+  const envBaseUrl = process.env['NX_API_BASE_URL'];
+  if (envBaseUrl) return envBaseUrl;
+  if (typeof window !== 'undefined')
+    return `https://${usage}${
+      location.href.includes('stage') ? '-stage' : ''
+    }.squoolr.com`;
+  return `https://${usage}-stage.squoolr.com`;
+}
+
+interface INavItem {
+  item: string;
+  route: string;
+}
+
+const landingNavElements: INavItem[] = [
+  { item: 'features', route: '#features' },
+  { item: 'partners', route: '#partners' },
+  { item: 'pricing', route: 'pricing' },
+  { item: 'faq', route: '#faq' },
+];
+
+export default function Navbar() {
+  const { formatMessage } = useIntl();
+  const { push, pathname, asPath } = useRouter();
+  const theme = useTheme();
+  const changeLanguage = useDispatchLanguage();
+  const activeLanguage = useActiveLanguage();
+
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: {
+          mobile: '1fr auto auto',
+          desktop: 'auto 1fr auto auto',
+        },
+        justifyItems: 'center',
+        columnGap: 1,
+        alignItems: 'center',
+        padding: {
+          mobile: '16px',
+          desktop: '11px 118px',
+        },
+        backgroundColor: '#EDF2FB',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: 'auto 1fr',
+          alignItems: 'center',
+          justifySelf: 'start',
+          cursor: 'pointer',
+        }}
+        onClick={() => push('/')}
+      >
+        <Image src="/logo.png" alt="Squoolr logo" height={68} width={68} />
+        <Typography variant="h3" sx={{ fontWeight: 'bold', color: '#12192C' }}>
+          Squoolr
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: {
+            desktop: 'grid',
+            mobile: 'none',
+            gridAutoFlow: 'column',
+            columnGap: '40px',
+            alignItems: 'center',
+          },
+        }}
+      >
+        {landingNavElements.map(({ item, route }, index) => (
+          <Typography
+            key={index}
+            sx={{
+              position: 'relative',
+              transition: '0.2s',
+              cursor: 'pointer',
+              '& a': {
+                textDecoration: 'none',
+              },
+              '&::before': {
+                transition: '0.2s',
+                position: 'absolute',
+                left: '0px',
+                bottom: '-5px',
+                height: '3px',
+                content: '""',
+                width:
+                  pathname === route ||
+                  `/${pathname.split('/').filter((_) => _ !== '')[0]}` ===
+                    route ||
+                  asPath.includes(route)
+                    ? '100%'
+                    : 0,
+                backgroundColor: theme.palette.primary.main,
+                borderRadius: '5px',
+              },
+              '&:hover::before': {
+                transition: '0.2s',
+                width: '100%',
+                backgroundColor:
+                  theme.palette[
+                    pathname === route ||
+                    `/${pathname.split('/').filter((_) => _ !== '')[0]}` ===
+                      route ||
+                    asPath.includes(route)
+                      ? 'secondary'
+                      : 'primary'
+                  ].main,
+              },
+            }}
+          >
+            <Link href={route}>
+              <Typography
+                component="span"
+                className="p3"
+                sx={{ color: 'var(--body)' }}
+              >
+                {formatMessage({ id: item })}
+              </Typography>
+            </Link>
+          </Typography>
+        ))}
+        <Typography
+          sx={{
+            position: 'relative',
+            transition: '0.2s',
+            cursor: 'pointer',
+            '& a': {
+              textDecoration: 'none',
+            },
+            '&::before': {
+              transition: '0.2s',
+              position: 'absolute',
+              left: '0px',
+              bottom: '-5px',
+              height: '3px',
+              content: '""',
+              width: 0,
+              backgroundColor: theme.palette.primary.main,
+              borderRadius: '5px',
+            },
+            '&:hover::before': {
+              transition: '0.2s',
+              width: '100%',
+              backgroundColor: theme.palette['primary'].main,
+            },
+          }}
+        >
+          <Typography
+            component="span"
+            className="p3"
+            sx={{ color: 'var(--body)' }}
+            onClick={() => alert('hello')}
+          >
+            {formatMessage({ id: 'contactUs' })}
+          </Typography>
+        </Typography>
+      </Box>
+      <FormControl
+        fullWidth
+        sx={{
+          '& .MuiSelect-select': {
+            paddingTop: 1,
+            paddingBottom: 1,
+          },
+        }}
+      >
+        <Select
+          value={activeLanguage}
+          onChange={() => {
+            changeLanguage({
+              type:
+                capitalize(activeLanguage) === 'En'
+                  ? 'USE_FRENCH'
+                  : 'USE_ENGLISH',
+            });
+          }}
+        >
+          <MenuItem value={'en'}>{formatMessage({ id: 'en' })}</MenuItem>
+          <MenuItem value={'fr'}>{formatMessage({ id: 'fr' })}</MenuItem>
+        </Select>
+      </FormControl>
+      <Tooltip arrow title={formatMessage({ id: 'menu' })}>
+        <IconButton
+          sx={{
+            display: {
+              mobile: 'block',
+              desktop: 'none',
+            },
+          }}
+          size="small"
+        >
+          <Icon
+            icon={list}
+            style={{
+              height: '32px',
+              width: '32px',
+              color: 'black',
+            }}
+          />
+        </IconButton>
+      </Tooltip>
+      <Box
+        sx={{
+          display: {
+            mobile: 'none',
+            desktop: 'block',
+          },
+        }}
+      >
+        <Button variant="text" color="primary" sx={{ fontSize: '16px' }}>
+          {formatMessage({ id: 'verifyDemandStatus' })}
+        </Button>
+        <Button variant="contained" color="primary">
+          {formatMessage({ id: 'getEarlyAccess' })}
+        </Button>
+      </Box>
+    </Box>
+  );
+}
