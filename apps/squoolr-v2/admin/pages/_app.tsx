9fabe34fc049794ae3b371f0f3e2721fd01b31a7
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-10-25 00:12:41 +0100

    feat(app): #57 configured _app with theme provider

--- apps/squoolr-v2/admin/pages/_app.tsx
+++ apps/squoolr-v2/admin/pages/_app.tsx
@@ -1,17 +1,56 @@
+import { GlomThemeProvider } from '@glom/theme';
+import { Box } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import { useEffect } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import '../public/styles/global.scss';
+import '../public/styles/notifGlobalStyles.css';
+import '../public/styles/phoneNumberStyles.css';
+import '../public/styles/reset.css';
+import '../public/styles/root.scss';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+  }, []);
+
   return (
-    <>
-      <Head>
-        <title>Welcome to admin!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
+    <GlomThemeProvider defaultLang="en">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Head>
+          <title>Squoolr Admin</title>
+        </Head>
+        <Box
+          component="main"
+          sx={{
+            minHeight: '100vh',
+            display: 'grid',
+            backgroundColor: '#FAFAFD',
+          }}
+        >
+          <Box
+            sx={{
+              height: '100%',
+              maxWidth: '1700px',
+              margin: '0 auto',
+              width: '100%',
+            }}
+          >
+            <Component {...pageProps} />
+          </Box>
+        </Box>
+      </LocalizationProvider>
+    </GlomThemeProvider>
   );
 }
 
