24daf88f0a99289e3a9f61170aaa41b53c73adcb
Marco Kuidja <kuidjamarco@gmail.com>
2023-10-28 15:06:20 +0100

    fix(server): #62 renamed departments create payload data types

--- apps/squoolr-v2/api/src/modules/departments/department.d.ts
+++ apps/squoolr-v2/api/src/modules/departments/department.d.ts
@@ -1,12 +1,12 @@
 import { Prisma } from '@prisma/client';
 
-export type CreateDepartmentInput = Pick<
+export type CreateDepartmentPayload = Pick<
   Prisma.DepartmentCreateManyInput,
   'department_acronym' | 'department_name' | 'school_id'
 >;
 
-export type UpdateDepartmentInput = Partial<
-  Omit<CreateDepartmentInput, 'school_id'> & {
+export type UpdateDepartmentPayload = Partial<
+  Omit<CreateDepartmentPayload, 'school_id'> & {
     is_deleted: true;
   }
 >;
