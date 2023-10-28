664a9a44953a4b80d5c394a227ed5d278dbb68fb
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-10-28 14:26:52 +0100

    feat(shared): #60 added sidenav component

---
+++ libs/squoolr-v2/side-nav/src/lib/SideNav.interfaces.ts
@@ -0,0 +1,12 @@
+import { IconifyIcon } from '@iconify/react';
+
+export interface INavItem {
+  icon: IconifyIcon;
+  title: string;
+  route: string;
+}
+
+export interface INavSection {
+  title: string;
+  navItems: INavItem[];
+}
