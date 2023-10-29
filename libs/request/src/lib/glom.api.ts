b6aaad45caa0b853f22bfa2aa79ed7cc940dc1d7
Marco Kuidja <kuidjamarco@gmail.com>
2023-10-29 20:40:20 +0100

    feat(shared): #63 updated glom api service

--- libs/request/src/lib/glom.api.ts
+++ libs/request/src/lib/glom.api.ts
@@ -1,10 +1,12 @@
+import { DemandsApi, InquiriesApi } from '../squoolr';
 import { GlomRequest, RequestParams } from './glom-request';
 
 export const GLOM_HOSTS = {
   lynkr: 'https://api.lynkr.net',
+  squoolr: 'https://api.squoolr.com',
 };
 export const getURI = (app: keyof typeof GLOM_HOSTS) => {
-  const DEV_PORT = 5000;
+  const DEV_PORT = 8000;
 
   const host =
     process.env.NODE_ENV === 'development'
@@ -15,11 +17,22 @@ export const getURI = (app: keyof typeof GLOM_HOSTS) => {
 };
 
 export class GlomApi {
+  public squoolr: {
+    demands: DemandsApi;
+    inquiries: InquiriesApi;
+  };
+
   constructor(public params?: Partial<RequestParams>) {
+    const { host } = getURI('squoolr');
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const lynkrRequest = new GlomRequest({
+    const squoolrRequest = new GlomRequest({
       ...params,
-      prefix: '/',
+      host,
     });
+
+    this.squoolr = {
+      demands: new DemandsApi(squoolrRequest),
+      inquiries: new InquiriesApi(squoolrRequest),
+    };
   }
 }
