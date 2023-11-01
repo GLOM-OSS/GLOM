7be0584e57e186ef9e687c8f15aa7bdfe4c53a40
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-11-01 22:27:15 +0100

    chore(shared): #60 exported breadcrumb components

--- libs/squoolr-v2/side-nav/src/index.ts
+++ libs/squoolr-v2/side-nav/src/index.ts
@@ -1,2 +1,5 @@
 export * from './lib/SideNav';
 export * from './lib/SideNav.interfaces';
+export * from './lib/SquoolrV2Layout';
+export * from './lib/breadcrumbContext/BreadcrumbContextProvider';
+export * from './lib/breadcrumbContext/BreadcrumbContext';
