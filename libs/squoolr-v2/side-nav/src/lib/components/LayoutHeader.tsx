cd020f758657b13cef8a4dc6cd562387e42d4e32
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-11-02 00:13:07 +0100

    feat(shared): #60 change title on nav click

--- libs/squoolr-v2/side-nav/src/lib/components/LayoutHeader.tsx
+++ libs/squoolr-v2/side-nav/src/lib/components/LayoutHeader.tsx
@@ -10,8 +10,10 @@ import {
 import { useIntl } from 'react-intl';
 import Breadcrumb from './Breadcrumb';
 import { useBreadcrumb } from '../breadcrumbContext/BreadcrumbContextProvider';
+import { IAppType } from '@glom/squoolr-v2/auth-ui';
+import Head from 'next/head';
 
-export default function LayoutHeader() {
+export default function LayoutHeader({ callingApp }: { callingApp: IAppType }) {
   const { formatMessage } = useIntl();
   const changeLanguage = useDispatchLanguage();
   const activeLanguage = useActiveLanguage();
@@ -19,48 +21,55 @@ export default function LayoutHeader() {
   const breadcrumbItems = useBreadcrumb();
 
   return (
-    <Box
-      sx={{
-        display: 'grid',
-        gridTemplateColumns: '1fr auto',
-        columnGap: 1,
-        alignItems: 'center',
-        borderBottom: `1px solid ${theme.common.line}`,
-        padding: '8px 40px',
-      }}
-    >
-      <Box>
-        <Typography variant="h1" sx={{ paddingBottom: '0 !important' }}>
-          {breadcrumbItems.length > 1
-            ? breadcrumbItems[1].title
-            : formatMessage({ id: 'noTitleAvailable' })}
-        </Typography>
-        <Breadcrumb />
-      </Box>
-      <FormControl
-        fullWidth
+    <>
+      <Head>
+        <title>{`Squoolr ${callingApp} ${
+          breadcrumbItems.length > 1 ? breadcrumbItems[1].title : ''
+        }`}</title>
+      </Head>
+      <Box
         sx={{
-          '& .MuiSelect-select': {
-            paddingTop: 1,
-            paddingBottom: 1,
-          },
+          display: 'grid',
+          gridTemplateColumns: '1fr auto',
+          columnGap: 1,
+          alignItems: 'center',
+          borderBottom: `1px solid ${theme.common.line}`,
+          padding: '8px 40px',
         }}
       >
-        <Select
-          value={activeLanguage}
-          onChange={() => {
-            changeLanguage({
-              type:
-                capitalize(activeLanguage) === 'En'
-                  ? 'USE_FRENCH'
-                  : 'USE_ENGLISH',
-            });
+        <Box>
+          <Typography variant="h1" sx={{ paddingBottom: '0 !important' }}>
+            {breadcrumbItems.length > 1
+              ? breadcrumbItems[1].title
+              : formatMessage({ id: 'noTitleAvailable' })}
+          </Typography>
+          <Breadcrumb />
+        </Box>
+        <FormControl
+          fullWidth
+          sx={{
+            '& .MuiSelect-select': {
+              paddingTop: 1,
+              paddingBottom: 1,
+            },
           }}
         >
-          <MenuItem value={'en'}>{formatMessage({ id: 'en' })}</MenuItem>
-          <MenuItem value={'fr'}>{formatMessage({ id: 'fr' })}</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+          <Select
+            value={activeLanguage}
+            onChange={() => {
+              changeLanguage({
+                type:
+                  capitalize(activeLanguage) === 'En'
+                    ? 'USE_FRENCH'
+                    : 'USE_ENGLISH',
+              });
+            }}
+          >
+            <MenuItem value={'en'}>{formatMessage({ id: 'en' })}</MenuItem>
+            <MenuItem value={'fr'}>{formatMessage({ id: 'fr' })}</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    </>
   );
 }
