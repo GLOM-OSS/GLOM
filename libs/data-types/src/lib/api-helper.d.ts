5da558cf0cf8a86d872572e1f925cbe687f2a242
Marco Kuidja <kuidjamarco@gmail.com>
2023-11-02 00:51:36 +0100

    perf(shared): improved swagger docs type inference helper perfomance

--- libs/data-types/src/lib/api-helper.d.ts
+++ libs/data-types/src/lib/api-helper.d.ts
@@ -1,19 +1,25 @@
 /**Shortcut to get Swagger schema response body */
 export type SchemaResponseBody<
   T,
-  K = keyof T
+  K extends keyof T
 > = T[K]['responses']['200']['content']['application/json'];
 
 /** Shortcut to get Swagger created(201) response body */
 export type SchemaCreateResponseBody<
   T,
-  K = keyof T
+  K extends keyof T
 > = T[K]['responses']['201']['content']['application/json'];
 
 export type SchemaRequestBody<
   T,
-  K = keyof T
+  K extends keyof T
 > = T[K]['requestBody']['content']['application/json'];
 
-export type SchemaRequestQuery<T, K = keyof T> = T[K]['parameters']['query'];
-export type SchemaRequestParams<T, K = keyof T> = T[K]['parameters']['path'];
+export type SchemaRequestQuery<
+  T,
+  K extends keyof T
+> = T[K]['parameters']['query'];
+export type SchemaRequestParams<
+  T,
+  K extends keyof T
+> = T[K]['parameters']['path'];
