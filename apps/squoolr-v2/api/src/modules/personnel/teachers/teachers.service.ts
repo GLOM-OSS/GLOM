af2ffb5148317bf3a072de62637507b6fb5558d6
Marco Kuidja <kuidjamarco@gmail.com>
2023-11-02 23:36:18 +0100

    feat(server): #66 added teachers findAll service method

---
+++ apps/squoolr-v2/api/src/modules/personnel/teachers/teachers.service.ts
@@ -0,0 +1,62 @@
+import { GlomPrismaService } from '@glom/prisma';
+import { Injectable } from '@nestjs/common';
+import { QueryParamsDto } from '../../modules.dto';
+import { getStaffWhereInput, getStaffSelect } from '../staff.service';
+import { StaffEntity } from '../staff.dto';
+import { IStaffService } from '../staff';
+import { QueryParams } from '../../module';
+import { Role } from '../../../app/auth/auth.decorator';
+
+@Injectable()
+export class TeachersService implements IStaffService<StaffEntity> {
+  constructor(private prismaService: GlomPrismaService) {}
+  findOne: (
+    academic_year_id: string,
+    params?: QueryParams
+  ) => Promise<StaffEntity>;
+
+  async findAll(academic_year_id: string, params?: QueryParamsDto) {
+    const teachers = await this.prismaService.annualTeacher.findMany({
+      select: {
+        annual_teacher_id: true,
+        Teacher: { select: { matricule: true } },
+        ...getStaffSelect(academic_year_id, Role.COORDINATOR, params),
+      },
+      where: getStaffWhereInput(academic_year_id, params),
+    });
+    return teachers.map(
+      ({
+        annual_teacher_id,
+        Teacher: { matricule },
+        Login: {
+          login_id,
+          Person,
+          Logs: [log],
+          AnnualConfigurators: [configrator],
+          AnnualRegistries: [registry],
+          AnnualTeachers: [{ AnnualClassroomDivisions: codinatedClass }],
+        },
+      }) =>
+        new StaffEntity({
+          login_id,
+          ...Person,
+          matricule,
+          annual_teacher_id,
+          last_connected: log?.logged_in_at ?? null,
+          roles: [{ registry }, { configrator }, { codinatedClass }].reduce<
+            Role[]
+          >(
+            (roles, _) =>
+              _.registry
+                ? [...roles, Role.REGISTRY]
+                : _.configrator
+                ? [...roles, Role.CONFIGURATOR]
+                : _.codinatedClass
+                ? [...roles, Role.COORDINATOR]
+                : roles,
+            [Role.TEACHER]
+          ),
+        })
+    );
+  }
+}
