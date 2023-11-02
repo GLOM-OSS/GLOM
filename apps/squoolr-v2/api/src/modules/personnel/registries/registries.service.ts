0b98734a6c8f11a25326d3255f6d2e1eb2f35967
Marco Kuidja <kuidjamarco@gmail.com>
2023-11-02 23:33:59 +0100

    feat(server): #66 added registries findAll service method

---
+++ apps/squoolr-v2/api/src/modules/personnel/registries/registries.service.ts
@@ -0,0 +1,59 @@
+import { GlomPrismaService } from '@glom/prisma';
+import { Injectable } from '@nestjs/common';
+import { getStaffWhereInput, getStaffSelect } from '../staff.service';
+import { QueryParamsDto } from '../../modules.dto';
+import { StaffEntity } from '../staff.dto';
+import { IStaffService } from '../staff';
+import { QueryParams } from '../../module';
+import { Role } from '../../../app/auth/auth.decorator';
+
+@Injectable()
+export class RegistriesService implements IStaffService<StaffEntity> {
+  constructor(private prismaService: GlomPrismaService) {}
+  findOne: (
+    academic_year_id: string,
+    params?: QueryParams
+  ) => Promise<StaffEntity>;
+
+  async findAll(academic_year_id: string, params?: QueryParamsDto) {
+    const registries = await this.prismaService.annualRegistry.findMany({
+      select: {
+        matricule: true,
+        annual_registry_id: true,
+        ...getStaffSelect(academic_year_id, Role.COORDINATOR, params),
+      },
+      where: getStaffWhereInput(academic_year_id, params),
+    });
+    return registries.map(
+      ({
+        annual_registry_id,
+        matricule,
+        Login: {
+          login_id,
+          Person,
+          Logs: [log],
+          AnnualConfigurators: [configrator],
+          AnnualTeachers: [teacher],
+        },
+      }) =>
+        new StaffEntity({
+          login_id,
+          ...Person,
+          matricule,
+          annual_registry_id,
+          last_connected: log?.logged_in_at ?? null,
+          roles: [{ configrator }, { teacher }].reduce<Role[]>(
+            (roles, _) =>
+              _.configrator
+                ? [...roles, Role.CONFIGURATOR]
+                : _.teacher
+                ? _.teacher.AnnualClassroomDivisions
+                  ? [...roles, Role.TEACHER, Role.COORDINATOR]
+                  : [...roles, Role.TEACHER]
+                : roles,
+            [Role.REGISTRY]
+          ),
+        })
+    );
+  }
+}
