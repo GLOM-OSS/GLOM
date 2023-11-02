2e049414ff3791b2c81aec13ab82559327cd95ed
Marco Kuidja <kuidjamarco@gmail.com>
2023-11-02 10:09:38 +0100

    fix(server): #31 added auth service to academic year module

--- apps/squoolr-v2/api/src/modules/academic-years/acdemic-years.controller.ts
+++ apps/squoolr-v2/api/src/modules/academic-years/acdemic-years.controller.ts
@@ -1,22 +1,31 @@
-import { Body, Controller, Get, Param, Patch, Post, Req } from '@nestjs/common';
-import { ApiExcludeEndpoint, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { Role, Roles } from '../../app/auth/auth.decorator';
+import { SessionEntity } from '../../app/auth/auth.dto';
+import { AuthenticatedGuard } from '../../app/auth/auth.guard';
+import { AuthService } from '../../app/auth/auth.service';
 import {
   AcademicYearEntity,
   CreateAcademicYearDto,
-  TemplateAcademicYearDto,
 } from './academic-years.dto';
 import { AcademicYearsService } from './academic-years.service';
-import { SessionEntity } from '../../app/auth/auth.dto';
-// import { AuthService } from '../auth/auth.service';
 
 @ApiTags('Academic Years')
 @Controller('academic-years')
-// @UseGuards(AuthenticatedGuard)
+@UseGuards(AuthenticatedGuard)
 export class AcademicYearsController {
   constructor(
-    // private authService: AuthService,
+    private authService: AuthService,
     private academicYearService: AcademicYearsService
   ) {}
 
@@ -54,13 +63,12 @@ export class AcademicYearsController {
     @Param('academic_year_id') academic_year_id: string
   ) {
     const { login_id } = request.session.passport.user;
-    // const { desirializedRoles, roles } =
     const { sessionData } = await this.academicYearService.selectAcademicYear(
       login_id,
       academic_year_id
     );
 
-    // await this.authService.updateSession(request, { roles, academic_year_id });
+    await this.authService.updateSession(request, { academic_year_id });
     return sessionData;
   }
 
