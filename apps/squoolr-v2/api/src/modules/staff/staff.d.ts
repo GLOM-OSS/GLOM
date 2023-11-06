8fbc69435579ae4048504899cc2e2f54a7c57ab9
Marco Kuidja <kuidjamarco@gmail.com>
2023-11-06 17:09:53 +0100

    fix(server): #66 updated staff creation interface

--- apps/squoolr-v2/api/src/modules/staff/staff.d.ts
+++ apps/squoolr-v2/api/src/modules/staff/staff.d.ts
@@ -12,20 +12,32 @@ export type StaffSelectParams = {
   activeRole?: StaffRole;
   academic_year_id?: string;
 };
-export type CreateStaffInput = Prisma.PersonCreateInput & {
-  academic_year_id: string;
-  password: string;
-  matricule: string;
-  person_id: string;
-  school_id: string;
+export type CreateStaffInput = Prisma.PersonCreateInput &
+  Pick<
+    Prisma.AnnualRegistryCreateManyInput,
+    'matricule' | 'private_code' | 'academic_year_id'
+  > &
+  Pick<Prisma.LoginCreateManyInput, 'password' | 'school_id'>;
+
+export type CreateTeacherInput = CreateStaffInput &
+  Omit<Prisma.TeacherCreateManyInput, 'teacher_id'> &
+  Pick<
+    Prisma.AnnualTeacherCreateManyInput,
+    | 'has_signed_convention'
+    | 'hourly_rate'
+    | 'origin_institute'
+    | 'teaching_grade_id'
+  >;
+export type CreateCoordinatorInput = {
+  annual_teacher_id: string;
+  classroomIds: string[];
 };
 
 export interface IStaffService<T> {
   findOne: (annual_personnel_id: string) => Promise<T>;
   findAll: (staffParams?: StaffSelectParams) => Promise<T[]>;
   create: (
-    payload: CreateStaffInput,
-    created_by: string,
-    private_code: string
+    payload: CreateStaffInput | CreateTeacherInput | CreateCoordinatorInput,
+    created_by: string
   ) => Promise<T>;
 }
