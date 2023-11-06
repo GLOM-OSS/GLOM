4c621f073be08323371b270f808e020ce8894f7c
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-11-06 14:20:59 +0100

    feat(app): #65 added reject demand dialog

---
+++ apps/squoolr-v2/admin/component/management/demands/RejectDemandDialog.tsx
@@ -0,0 +1,104 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from '@mui/material';
+import { DialogTransition } from '@squoolr/confirm-dialogs';
+import { theme } from '@glom/theme';
+import { useFormik } from 'formik';
+import { useIntl } from 'react-intl';
+import * as Yup from 'yup';
+
+export default function RejectDemandDialog({
+  isDialogOpen,
+  closeDialog,
+  handleSubmit,
+}: {
+  isDialogOpen: boolean;
+  closeDialog: () => void;
+  handleSubmit: (reason: string) => void;
+}) {
+  const { formatMessage } = useIntl();
+  const initialValues: { reason: string } = {
+    reason: '',
+  };
+  const validationSchema = Yup.object().shape({
+    reason: Yup.string().required(formatMessage({ id: 'reasonRequired' })),
+  });
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values, { resetForm }) => {
+      handleSubmit(values.reason);
+      resetForm();
+      closeDialog();
+    },
+  });
+
+  return (
+    <Dialog
+      open={isDialogOpen}
+      TransitionComponent={DialogTransition}
+      keepMounted
+      onClose={closeDialog}
+      sx={{
+        '& .MuiPaper-root': {
+          padding: { laptop: '2% 10%', mobile: 0 },
+        },
+      }}
+    >
+      <DialogTitle sx={{ textAlign: 'center' }}>
+        {formatMessage({ id: 'rejectDemand' })}
+      </DialogTitle>
+      <form onSubmit={formik.handleSubmit}>
+        <DialogContent>
+          <DialogContentText sx={{ marginBottom: theme.spacing(2) }}>
+            {formatMessage({ id: 'confirmRejectionMessage' })}
+          </DialogContentText>
+          <TextField
+            autoFocus
+            placeholder={formatMessage({ id: 'rejectionReason' })}
+            fullWidth
+            required
+            multiline
+            color="error"
+            rows={4}
+            error={formik.touched.reason && Boolean(formik.errors.reason)}
+            helperText={formik.touched.reason && formik.errors.reason}
+            {...formik.getFieldProps('reason')}
+          />
+        </DialogContent>
+        <DialogActions
+          sx={{
+            justifyContent: 'center',
+            display: 'grid',
+            gridAutoFlow: 'column',
+            columnGap: '20px',
+          }}
+        >
+          <Button
+            sx={{ textTransform: 'none' }}
+            color="inherit"
+            variant="outlined"
+            onClick={closeDialog}
+          >
+            {formatMessage({ id: 'cancel' })}
+          </Button>
+          <Button
+            sx={{ textTransform: 'none' }}
+            color="error"
+            variant="contained"
+            type="submit"
+          >
+            {formatMessage({ id: 'reject' })}
+          </Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+}
