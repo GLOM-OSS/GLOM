f326cedb202aab82ce938a55bdf50376f599a188
Marco Kuidja <kuidjamarco@gmail.com>
2023-11-07 10:14:01 +0100

    feat(server): #66 implemented coordo create method

--- apps/squoolr-v2/api/src/modules/staff/coordinators/coordinators.service.ts
+++ apps/squoolr-v2/api/src/modules/staff/coordinators/coordinators.service.ts
@@ -8,14 +8,11 @@ import {
 } from '../staff';
 import { StaffArgsFactory } from '../staff-args.factory';
 import { StaffEntity } from '../staff.dto';
+import { BatchPayloadDto } from '../../modules.dto';
 
 @Injectable()
 export class CoordinatorsService implements IStaffService<StaffEntity> {
   constructor(private prismaService: GlomPrismaService) {}
-  create: (
-    payload: CreateCoordinatorInput,
-    created_by: string
-  ) => Promise<StaffEntity>;
   async findOne(annual_coordinator_id: string): Promise<StaffEntity> {
     throw new NotImplementedException(
       '`findOne` method is not supported for coordinators. Use teacher instead'
@@ -70,5 +67,46 @@ export class CoordinatorsService implements IStaffService<StaffEntity> {
           ),
         })
     );
+  }
+
+  async create(
+    { annual_teacher_id, annualClassroomIds }: CreateCoordinatorInput,
+    created_by: string
+  ) {
+    const annualClassroomDivisions =
+      await this.prismaService.annualClassroomDivision.findMany({
+        select: {
+          annual_classroom_division_id: true,
+          annual_coordinator_id: true,
+          is_deleted: true,
+        },
+        where: {
+          OR: annualClassroomIds.map((annual_classroom_id) => ({
+            annual_classroom_id,
+          })),
+        },
+      });
+    const [{ count }] = await this.prismaService.$transaction([
+      this.prismaService.annualClassroomDivision.updateMany({
+        data: {
+          annual_coordinator_id: annual_teacher_id,
+        },
+        where: {
+          OR: annualClassroomIds.map((annual_classroom_id) => ({
+            annual_classroom_id,
+          })),
+        },
+      }),
+      this.prismaService.annualClassroomDivisionAudit.createMany({
+        data: annualClassroomDivisions.map((data) => ({
+          ...data,
+          audited_by: created_by,
+        })),
+      }),
+    ]);
+    return new BatchPayloadDto({
+      count,
+      message: `Updated ${count} records in database`,
+    });
   }
 }
