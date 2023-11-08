d88367275202e5ec016487921890bbf9516495ad
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-11-08 13:12:53 +0100

    feat(shared): #65 made tableHeaderItem cmpt

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/PhoneTextField';
 export * from './lib/404';
 export * from './table/NoTableElement';
+export * from './table/TableHeaderItem';
 export * from './dialog/ConfirmDialog';
 export * from './dialog/dialog-transition';
