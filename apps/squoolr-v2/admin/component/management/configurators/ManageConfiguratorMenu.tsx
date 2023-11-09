131e87090e9b2d3a6733b473bdd62a6503a0b3a3
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-11-09 01:06:24 +0100

    feat(app): #71 added admin configurators page

---
+++ apps/squoolr-v2/admin/component/management/configurators/ManageConfiguratorMenu.tsx
@@ -0,0 +1,71 @@
+import { useTheme } from '@glom/theme';
+import trash from '@iconify/icons-fluent/delete-48-regular';
+import lock from '@iconify/icons-fluent/lock-closed-48-regular';
+import { Icon } from '@iconify/react';
+import { Menu, MenuItem, Stack, Typography } from '@mui/material';
+import { useIntl } from 'react-intl';
+
+export default function ManageConfiguratorMenu({
+  closeMenu,
+  anchorEl,
+  isOpen,
+  onDisableAccount,
+  onResetPassword,
+}: {
+  closeMenu: () => void;
+  anchorEl: HTMLElement | null;
+  isOpen: boolean;
+  onDisableAccount: () => void;
+  onResetPassword: () => void;
+}) {
+  const { formatMessage } = useIntl();
+  const theme = useTheme();
+
+  return (
+    <Menu
+      anchorEl={anchorEl}
+      transformOrigin={{
+        vertical: 12,
+        horizontal: 170,
+      }}
+      open={isOpen}
+      onClose={closeMenu}
+      sx={{
+        '& .MuiPaper-root': {
+          borderRadius: '5px',
+          boxShadow: 'none',
+          border: `1px solid ${theme.common.line}`,
+        },
+      }}
+    >
+      <MenuItem
+        onClick={() => {
+          closeMenu();
+          onResetPassword();
+        }}
+      >
+        <Stack direction="row" spacing={1} alignItems={'center'}>
+          <Icon icon={lock} fontSize={24} />
+          <Typography>{formatMessage({ id: 'resetPassword' })}</Typography>
+        </Stack>
+      </MenuItem>
+      <MenuItem
+        onClick={() => {
+          onDisableAccount();
+          closeMenu();
+        }}
+      >
+        <Stack direction="row" spacing={1} alignItems={'center'}>
+          <Icon
+            icon={trash}
+            fontSize={24}
+            style={{ color: theme.palette.warning.main }}
+          />
+          <Typography sx={{ color: theme.palette.warning.main }}>
+            {formatMessage({ id: 'disableAccount' })}
+          </Typography>
+        </Stack>
+      </MenuItem>
+    </Menu>
+  );
+}
