cab5776f40770db94b4ae4e7f87d37f2d1d63d49
Marco Kuidja <kuidjamarco@gmail.com>
2023-11-13 01:14:55 +0100

    feat(shared): #79 prepared auth api for integrations

--- libs/data-access/squoolr/src/index.ts
+++ libs/data-access/squoolr/src/index.ts
@@ -1,6 +1,8 @@
 export * from './lib/ambassadors.api';
+export * from './lib/auth.api';
 export * from './lib/demands.api';
 export * from './lib/inquiries.api';
 export * from './lib/platform-settings.api';
 export * from './lib/staffs.api';
+
 
