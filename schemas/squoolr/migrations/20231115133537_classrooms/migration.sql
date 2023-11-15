d2703972e8b3bbf13ca9c11d5b8c6801c32958a9
Marco Kuidja <kuidjamarco@gmail.com>
2023-11-15 14:41:16 +0100

    feat(server): #62 removed `classroom_level` on AnnnualMajor model

---
+++ schemas/squoolr/migrations/20231115133537_classrooms/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `classroom_level` on the `annualclassroomaudit` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `annualclassroomaudit` DROP COLUMN `classroom_level`;
