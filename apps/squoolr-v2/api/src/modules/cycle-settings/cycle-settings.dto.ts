327fe04fa12dcd5d50f42ce1688609efff8327b1
Marco Kuidja <kuidjamarco@gmail.com>
2023-11-17 19:08:20 +0100

    feat(server): #85 preparing exam access get and create method DTOs

---
+++ apps/squoolr-v2/api/src/modules/cycle-settings/cycle-settings.dto.ts
@@ -0,0 +1,61 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { AnnualSemesterExamAcess } from '@prisma/client';
+import { Exclude, Type } from 'class-transformer';
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsIn,
+  IsPositive,
+  Max,
+  ValidateNested
+} from 'class-validator';
+
+export class ExamAcessSettingPayload {
+  @IsIn([1, 2])
+  @ApiProperty()
+  annual_semester_number: number;
+
+  @Max(100)
+  @IsPositive()
+  @ApiProperty()
+  payment_percentage: number;
+
+  constructor(props: ExamAcessSettingPayload) {
+    Object.assign(this, props);
+  }
+}
+
+export class UpdateExamAcessSettingDto {
+  @ArrayMaxSize(2)
+  @ArrayMinSize(2)
+  @ValidateNested({ each: true })
+  @Type(() => ExamAcessSettingPayload)
+  @ApiProperty({ type: [ExamAcessSettingPayload, ExamAcessSettingPayload] })
+  payload: [ExamAcessSettingPayload, ExamAcessSettingPayload];
+}
+
+export class ExamAccessSettingEntitty
+  extends ExamAcessSettingPayload
+  implements AnnualSemesterExamAcess
+{
+  @ApiProperty()
+  annual_semester_exam_access_id: string;
+
+  @ApiProperty()
+  created_at: Date;
+
+  @ApiProperty()
+  cycle_id: string;
+
+  @ApiProperty()
+  academic_year_id: string;
+
+  @Exclude()
+  @ApiProperty()
+  created_by: string;
+
+  constructor(props: ExamAccessSettingEntitty) {
+    super(props);
+    Object.assign(this, props);
+  }
+}
