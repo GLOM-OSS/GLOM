b0cbdeeb04bb91709ce59ea51ce04abd4f6042ec
Marco Kuidja <kuidjamarco@gmail.com>
2023-11-23 13:43:05 +0100

    fix(shared): #94 adjusted redis connection config

--- libs/nest/redis/src/lib/glom-redis.module.ts
+++ libs/nest/redis/src/lib/glom-redis.module.ts
@@ -1,28 +1,22 @@
-import { Module, Provider } from '@nestjs/common';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Module } from '@nestjs/common';
+import { redisStore } from 'cache-manager-ioredis-yet';
 import { GlomRedisModuleOptions } from './glom-redis';
 import { GlomRedisService } from './glom-redis.service';
-import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-ioredis-yet';
-import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({})
 export class GlomRedisModule {
   static async forRoot({ isGlobal = false, config }: GlomRedisModuleOptions) {
-    const providers: Provider[] = [
-      {
-        provide: GlomRedisService,
-        useValue: new GlomRedisService(config.url, config),
-      },
-    ];
-
     return {
-      providers,
       global: isGlobal,
-      exports: providers,
       module: GlomRedisModule,
+      providers: [GlomRedisService],
+      exports: [GlomRedisService],
       imports: [
         RedisModule.forRoot({
           config: {
+            ...config,
             url: config.url,
           },
         }),
--- libs/nest/redis/src/lib/glom-redis.service.ts
+++ libs/nest/redis/src/lib/glom-redis.service.ts
@@ -1,7 +1,8 @@
+import { InjectRedis } from '@nestjs-modules/ioredis';
 import { Redis, RedisOptions } from 'ioredis';
 
-export class GlomRedisService extends Redis {
-  constructor(url: string, options: RedisOptions) {
-    super(url, options);
+export class GlomRedisService {
+  constructor(@InjectRedis() redis: Redis) {
+    Object.assign(this, redis);
   }
 }
