7361c6c73a134ea0f1ab445912604c7da7749912
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-11-27 19:48:50 +0100

    testing

--- apps/squoolr-v2/admin/pages/management/schools/[school_id]/index.tsx
+++ apps/squoolr-v2/admin/pages/management/schools/[school_id]/index.tsx
@@ -94,13 +94,14 @@ export default function index() {
   const breadcrumbDispatch = useDispatchBreadcrumb();
   const breadcrumbs = useBreadcrumb();
   useEffect(() => {
+    const doesBreadcrumbHaveItem = breadcrumbs.find(
+      ({ route }) =>
+        route && route.includes(schoolData?.school.school_code as string)
+    );
     if (!!schoolData) {
       const {
         school: { school_demand_status, school_code, school_acronym },
       } = schoolData;
-      const doesBreadcrumbHaveItem = breadcrumbs.find(
-        ({ route }) => route && route.includes(schoolData.school.school_code)
-      );
 
       if (!doesBreadcrumbHaveItem)
         breadcrumbDispatch({
@@ -127,10 +128,11 @@ export default function index() {
           });
       }, 2 * 60 * 1000);
     } else {
-      breadcrumbDispatch({
-        action: 'ADD',
-        payload: [{ title: formatMessage({ id: 'loading' }), route: asPath }],
-      });
+      if (!doesBreadcrumbHaveItem)
+        breadcrumbDispatch({
+          action: 'ADD',
+          payload: [{ title: formatMessage({ id: 'loading' }), route: asPath }],
+        });
     }
   }, [schoolData]);
 
