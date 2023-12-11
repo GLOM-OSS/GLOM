8b1209958b85be5567d7f26259ed0e350c536ef2
Marco Kuidja <kuidjamarco@gmail.com>
2023-12-11 21:25:34 +0100

    feat(shared): #105 exposed secretary APIs

--- libs/request/src/squoolr/index.ts
+++ libs/request/src/squoolr/index.ts
@@ -1,8 +1,13 @@
 export * from './academic-years.api';
 export * from './ambassadors.api';
 export * from './auth.api';
-export * from './schools.api';
+export * from './classrooms.api';
+export * from './departments.api';
+export * from './helpers.api';
 export * from './inquiries.api';
-export * from './platform-settings.api';
-export * from './staffs.api';
+export * from './majors.api';
 export * from './payments.api';
+export * from './platform-settings.api';
+export * from './schools.api';
+export * from './staffs.api';
+
